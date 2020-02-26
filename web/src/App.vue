80fae4ad7a5630cec93e497cb5353ed682d3a686
Laura Hughes <flaneuse@users.noreply.github.com>
2020-02-26 11:24:45 -0800

    add custom fonts

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,16 +2,16 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/epidemiology">Epidemiology</router-link>
     </div>
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
-
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: $font-family;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
