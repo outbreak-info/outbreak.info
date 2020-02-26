962c5216a27c7b01c30b9d9537cebcca73650e71
Laura Hughes <flaneuse@users.noreply.github.com>
2020-02-26 14:58:03 -0800

    temp: basic header

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,33 +1,44 @@
 <template>
-  <div id="app">
-    <div id="nav">
+<div id="app">
+  <header id="outbreak-header">
+    outbreak.info
+    <nav id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/epidemiology">Epidemiology</router-link>
-    </div>
-    <router-view />
-  </div>
+    </nav>
+  </header>
+
+
+  <router-view />
+</div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: $font-family;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: $font-family;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+}
+
+#outbreak-header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 }
 
 #nav {
-  padding: 30px;
+    padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+    a {
+        font-weight: bold;
+        color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: $primary-color;
+        &.router-link-exact-active {
+            color: $primary-color;
+        }
     }
-  }
 }
 </style>
