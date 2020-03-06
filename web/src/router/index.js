e3693536dfb8da88bc907201aa6a8ee3f3c5e8fe
Laura Hughes <flaneuse@users.noreply.github.com>
2020-03-06 14:33:05 -0800

    update routes

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -17,6 +17,18 @@ const routes = [{
     // which is lazy-loaded when the route is visited.
     component: () =>
       import( /* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/epidemiology",
+    name: "Epidemiology",
+    props: route => ({
+      region: route.query.region
+    }),
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import( /* webpackChunkName: "about" */ "../views/Epi.vue")
   }
 ];
 
