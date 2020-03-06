d279be2ec895d486790cb6b5600ffd1ef442d89b
Laura Hughes <flaneuse@users.noreply.github.com>
2020-03-06 14:05:39 -0800

    Add Google Analytics

--- web/src/main.js
+++ web/src/main.js
@@ -2,8 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueGtag from "vue-gtag";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueGtag, {
+  config: {
+    id: "UA-159949707-1"
+  }
+}, router);
 
 new Vue({
   router,
