53bb91614c8422ea793b8ccb9c85928bd6d54939
Laura Hughes <flaneuse@users.noreply.github.com>
2020-03-07 20:57:54 -0800

    fix syntax error

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -33,7 +33,7 @@
     <DataSource />
   </section>
   <section class="case-data-table">
-    <EpiTable :data="cases" routable="true"/>
+    <EpiTable :data="cases" :routable="true"/>
   </section>
 </div>
 </template>
