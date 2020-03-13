a5131b16df4cf4e632c17d53d55f9980f76f974b
Marco <artofmarco@gmail.com>
2020-03-13 15:16:49 -0700

    nav issues fixed, images contrast imrpoved, rearranged content, added temp warning, other small fixes

---
+++ web/src/components/Warning.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="alert bg-highlight text-light p-2 row m-0 rounded-0" :class="{'scale-in-center':animate}">
+    <div class="col-sm-1 d-flex justify-content-center align-items-center text-center">
+      <i class="fas fa-exclamation-circle pr-2"></i>
+    </div>
+    <div class="col-sm-11 d-flex justify-content-center align-items-center text-center">
+      <p class="m-0">
+        {{text}}
+      </p>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "SearchBar",
+  props: {
+    text: String,
+    animate: Boolean
+  }
+});
+</script>
