be8d18bb0542953e2629a2aa9d5974d79909fb36
Laura Hughes <flaneuse@users.noreply.github.com>
2020-03-22 12:26:20 -0700

    switch to Biothings API

--- web/src/main.js
+++ web/src/main.js
@@ -4,15 +4,17 @@ import router from "./router";
 import store from "./store";
 import VueGtag from "vue-gtag";
 import VueRx from "vue-rx";
+import VueCookies from 'vue-cookies'
 
 import "leaflet/dist/leaflet.css";
 import "tippy.js/dist/tippy.css";
 
 Vue.config.productionTip = false;
 // Vue.prototype.$apiurl = "http://kenny.scripps.edu:8000/";
-Vue.prototype.$apiurl = "http://pending.biothings.io/covid19/";
+Vue.prototype.$apiurl = "http://api.outbreak.info/v1/";
 
 Vue.use(VueRx);
+Vue.use(VueCookies);
 
 Vue.use(
   VueGtag,
