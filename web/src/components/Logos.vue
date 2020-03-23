5fcf1ebde3346c8fa29d6348e63abfef113859a3
Marco <artofmarco@gmail.com>
2020-03-23 10:32:40 -0700

    added logos and about page updated, intro banner smaller

--- web/src/components/Logos.vue
+++ web/src/components/Logos.vue
@@ -1,21 +1,37 @@
 <template>
-  <div class="row m-0 bg-grey">
-    <div
-      class="col-md-6 col-sm-12 d-flex justify-content-center align-items-center"
-    >
+  <div class="row m-0 bg-grey d-flex justify-content-center align-items-center">
+    <div class="col-sm-2 d-flex justify-content-center align-items-center">
+      <a href="http://sulab.org/" rel="noreferrer" target="_blank">
+        <img src="@/assets/sulab-logo.png" class="w-100" alt="SuLab" />
+      </a>
+    </div>
+    <div class="col-sm-2 d-flex justify-content-center align-items-center">
+      <a href="http://sulab.org/" rel="noreferrer" target="_blank">
+        <img src="@/assets/wu-logo.png" class="w-100" alt="WuLab" />
+      </a>
+    </div>
+    <div class="col-sm-2 d-flex justify-content-center align-items-center">
+      <a href="https://andersen-lab.com/" rel="noreferrer" target="_blank">
+        <img src="@/assets/anderson-logo.png" class="w-100" alt="Anderson Lab" />
+      </a>
+    </div>
+    <div class="col-sm-2 d-flex justify-content-center align-items-center">
       <a href="scripps.edu" rel="noreferrer" target="_blank">
         <img
           src="@/assets/scripps-logo.png"
-          width="200px"
+          class="w-100"
           alt="Scripps Research"
         />
       </a>
     </div>
-    <div
-      class="col-md-6 col-sm-12 d-flex justify-content-center align-items-center"
-    >
-      <a href="http://sulab.org/" rel="noreferrer" target="_blank">
-        <img src="@/assets/sulab-logo.png" width="200px" alt="SuLab" />
+    <div class="col-sm-2 d-flex justify-content-center align-items-center">
+      <a href="https://cvisb.org/" rel="noreferrer" target="_blank">
+        <img src="@/assets/cvisb-logo.png" class="w-100" alt="CVISB" />
+      </a>
+    </div>
+    <div class="col-sm-2 d-flex justify-content-center align-items-center">
+      <a href=" https://globalhealth.scripps.edu/" rel="noreferrer" target="_blank">
+        <img src="@/assets/global-logo.png" class="w-100" alt="SDCGH" />
       </a>
     </div>
   </div>
