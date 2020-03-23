b5f45e5004a2babe36a912edb052b884bd788ee4
Marco <artofmarco@gmail.com>
2020-03-23 15:06:31 -0700

    privacy policy page added

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Privacy from "../views/Privacy.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home
+  },
+  {
+    path: "/privacy-policy",
+    name: "Privacy",
+    component: Privacy
   },
   {
     path: "/about",
