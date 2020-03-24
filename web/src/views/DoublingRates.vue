165b6d99e76e29dcc4ba14381914726ea8407f7f
Laura Hughes <flaneuse@users.noreply.github.com>
2020-03-24 12:24:15 -0700

    prelim button interface to change points

--- web/src/views/DoublingRates.vue
+++ web/src/views/DoublingRates.vue
@@ -1,10 +1,10 @@
 <template>
 <div>
 
-<div class="d-flex align-items-center mx-4">
-  <DoublingCurve v-if="epi$" :data="epi$" :toFit="toFit"/>
-  <DoublingTable v-if="epi$" :data="epi$" @changeFit="changeFit"/>
-</div>
+  <div class="d-flex align-items-center mx-4">
+    <DoublingCurve v-if="epi$" :data="epi$" :toFit="toFit" @executeFit="executeFit" />
+    <DoublingTable v-if="epi$" :data="epi$" :isFitting1="fitting1" :isFitting2="fitting2" @changeFit="changeFit" />
+  </div>
 
 </div>
 </template>
@@ -12,7 +12,9 @@
 <script lang="ts">
 import Vue from "vue";
 import {
-  getDoubling, getAllDoublingBatch, getAllDoubling
+  getDoubling,
+  getAllDoublingBatch,
+  getAllDoubling
 } from "@/api/calc-doubling.js";
 
 import DoublingCurve from "@/components/DoublingCurve.vue"
@@ -27,19 +29,29 @@ export default Vue.extend({
   },
   data() {
     return {
-      toFit: null
+      toFit: null,
+      fitting1: false,
+      fitting2: true,
+      // set up initial plot indices
+      // (this.plottedData.length - this.numFit)
+      // this.plottedData.length - this.numFit * 2 && i < this.plottedData.length - this.numFit)
+      // this.fitIdx1 = [23,24,25,26];
+      // this.fitIdx2 = [23,24,25,26];
     }
   },
   subscriptions() {
     return {
-      epi$: getDoubling(this.$apiurl, "GBR"),
+      epi$: getDoubling(this.$apiurl, "DEU"),
       // all$: getAllDoubling(this.$apiurl)
     }
   },
   methods: {
-changeFit: function(fitIdx){
-  console.log(fitIdx)
-}
+    changeFit: function(fitIdx) {
+      this.toFit = fitIdx;
+    },
+    executeFit: function(fitIdx) {
+      this[`fitting${fitIdx}`] = !this[`fitting${fitIdx}`];
+    }
   },
   mounted() {
     console.log('hi');
