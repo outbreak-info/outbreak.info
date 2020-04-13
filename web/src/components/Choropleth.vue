26d1bf60f24ebf001d0ad7888638345a71c8bc96
Laura Hughes <flaneuse@users.noreply.github.com>
2020-04-13 15:39:42 -0700

    add basic tooltips

--- web/src/components/Choropleth.vue
+++ web/src/components/Choropleth.vue
@@ -5,6 +5,10 @@
     <g ref="regions" class="region-group"></g>
     <g ref="states" class="state-group"></g>
   </svg>
+  <div class="tooltip choropleth-tooltip p-2" ref="choropleth_tooltip">
+    <h6 class="country-name m-0"></h6>
+    <p class="value m-0"></p>
+  </div>
   <HistogramLegend :data="data" :variable="variable" :colorScale="colorScale" />
 </div>
 </template>
@@ -65,6 +69,7 @@ export default {
       this.svg = d3.select(this.$refs.svg);
       this.states = d3.select(this.$refs.states);
       this.regions = d3.select(this.$refs.regions);
+      this.ttips = d3.select(this.$refs.choropleth_tooltip);
     },
     drawMetro() {
       if (this.data) {
@@ -74,16 +79,18 @@ export default {
           const idx = geodata.features.findIndex(polygon => polygon.properties.GEOID === id);
           if (idx > -1) {
             geodata.features[idx]["fill"] = d.fill;
-
+            geodata.features[idx]["location_id"] = d.location_id;
+            geodata.features[idx]["name"] = d.name;
+            geodata.features[idx]["value"] = d3.format(".1f")(d[this.variable]);
           }
         })
-
 
         var path = d3.geoPath();
         var projection = d3.geoAlbersUsa()
           .scale(700)
           .translate([this.width / 2 + 10, this.height / 2]);
 
+        // regional data
         this.regions
           .selectAll("path")
           .data(geodata.features)
@@ -92,6 +99,7 @@ export default {
               enter
                 .append("path")
                 .attr("class", "region")
+                .attr("id", d => d.location_id)
                 // draw each region
                 .attr("d", path
                   .projection(projection)
@@ -101,6 +109,7 @@ export default {
             update => update.attr("fill", d => d.fill ? d.fill : "none")
           )
 
+        // state outline
         this.states
           .selectAll("path")
           .data(usstates.features)
@@ -114,8 +123,31 @@ export default {
                   .projection(projection)
                 )
             }
-          )
+          );
+
+        // tooltip
+        this.regions.selectAll("path.region")
+          .on("mouseenter", d => this.mouseOn(d))
+          .on("mouseleave", this.mouseOff);
+
       }
+    },
+    mouseOn(d) {
+      const ttip = this.ttips
+        .style("top", d3.event.y + "px")
+        .style("left", d3.event.x + "px")
+        .style("opacity", 1);
+
+      this.regions.selectAll("path.region").style("opacity", 0.5);
+      this.regions.selectAll(`#${d.location_id}`).style("opacity", 1);
+
+      this.ttips.select(".country-name").text(d.name);
+      this.ttips.select(".value").text(d.value);
+    },
+    mouseOff() {
+      d3.selectAll(".tooltip")
+        .style("opacity", 0);
+      this.regions.selectAll("path.region").style("opacity", 1);
     }
 
   }
@@ -141,5 +173,13 @@ export default {
     fill: none;
     stroke: $grey-90;
     stroke-width: 0.5;
+}
+
+.choropleth-tooltip {
+    position: fixed;
+    z-index: 1000;
+    background: #ffffff;
+    opacity: 0;
+    pointer-events:none;
 }
 </style>
