0c8467824636f246d614a6db9cf04b80e6c77eab
Laura Hughes <flaneuse@users.noreply.github.com>
2020-05-01 16:39:59 -0700

    link what's new to dataset page

--- web/src/views/Resources.vue
+++ web/src/views/Resources.vue
@@ -45,13 +45,19 @@
 
         <table id='whats-new'>
           <tbody>
-            <tr v-for="(item, idx) in newData" :key="idx" class="new-item">
-              <td class="resource-type d-flex align-items-center" :class="item['@type']">
-                <StripeAccent :className="item['@type']" />
+            <tr v-for="(item, idx) in newData" :key="idx" class="new-item d-flex">
+              <td class="resource-type d-flex align-items-center">
+                <router-link :to="{ name: 'Resource Page', params: {id: item._id} }" class="no-underline m-0" :class="item['@type']">
+                  <StripeAccent :className="item['@type']" />
 
-                {{item['@type']}}
+                  {{item['@type']}}
+                </router-link>
               </td>
-              <td class="resource-name text-left" valign="top">{{item.name}}</td>
+              <td class="resource-name text-left" valign="top">
+                <router-link :to="{ name: 'Resource Page', params: {id: item._id} }" class="no-underline m-0 text-dark">
+                  {{item.name}}
+                </router-link>
+              </td>
               <td class="resource-affiliation text-left text-muted" valign="top">
                 <template v-if="item.author">
                   {{item.author[0].name ? item.author[0].name : item.author[0].givenName + " " + item.author[0].familyName}}
@@ -63,7 +69,6 @@
                 </template>
               </td>
               <td class="resource-date" valign="top">{{format(item.date)}}</td>
-
             </tr>
 
           </tbody>
@@ -498,302 +503,6 @@ export default {
         id: "dataset"
       }, ],
       new2Display: 3,
-      data2: [{
-          _id: "virological1",
-          descriptionExpanded: false,
-          "@type": "Analysis",
-          url: "http://virological.org/t/preliminary-in-silico-assessment-of-the-specificity-of-published-molecular-assays-and-design-of-new-assays-using-the-available-whole-genome-sequences-of-2019-ncov/343",
-          name: "Preliminary in silico assessment of the specificity of published molecular assays and design of new assays using the available whole genome sequences of 2019-nCoV",
-          author: [{
-            name: "Mitchell Holland",
-            affiliation: "Noblis, Reston, VA 20191"
-          }, {
-            name: "Daniel Negrón",
-            affiliation: "Noblis, Reston, VA 20191"
-          }],
-          mainEntity: "http://virological.org/",
-          dateCreated: "2020-01-24",
-          dateModified: "2020-03-29",
-          lastReviewed: "2020-04-01",
-          relatedTo: ["http://virological.org/t/initial-assessment-of-the-ability-of-published-coronavirus-primers-sets-to-detect-the-wuhan-coronavirus/321Source"],
-          analysisTechnique: "Whole Genome Sequencing",
-          variableMeasured: "", // something similiar?
-          keywords: ["Whole Genome Sequencing", "Assay Design"],
-          infectiousAgent: "SARS-CoV-2",
-          description: "BioLaboro is an application for rapidly designing de novo assays and validating existing PCR detection assays. It is a user-friendly new assay discovery pipeline composed of three tools: BioVelocity®, Primer3, and PSET. BioVelocity® uses a rapid, accurate hashing algorithm to align sequencing reads to a large set of references (e.g. Genbank) (Sozhamannan et al., 2015). BioVelocity® creates a k-mer index to determine all possible matches between query sequences and references simultaneously using a large RAM system (i.e. an IBM Power8). This algorithm makes it possible to very quickly identify sequences conserved within or omitted from a set of target references. Primer3 (http://primer3.sourceforge.net/ 29) is a tool for designing primers and probes for real-time PCR reactions. It considers a range of criteria such as oligonucleotide melting temperature, size, GC content, and primer-dimer possibilities. We use Primer3 along with our signature detection process to identify potential new primer sets. PSET (PCR Signature Erosion Tool) tests PCR assays in silico against the latest versions of public sequence repositories, or other reference datasets, to determine if primers and probes match only to their intended targets. Using this information, an assay provider can be better aware of potential false hits and be better prepared to design new primers when false hits become intractable.",
-        }, {
-          _id: "nejm1",
-          "@type": "Publication",
-          descriptionExpanded: false,
-          url: "http://doi.org/10.1056/NEJMc2007942",
-          name: "Stability and Viability of SARS-CoV-2",
-          identifier: "1533-4406",
-          doi: "10.1056/NEJMc2007942",
-          pmid: "32283575",
-          datePublished: "2020-04-13",
-          journalName: "The New England journal of medicine",
-          journalNameAbbrev: "N Engl J Med.",
-          volumeNumber: 382,
-          publicationType: ["Letter", "Comment"],
-          abstract: "The letter by van Doremalen et al. (published March 17 at NEJM.org)1 provides important information on the viability of severe acute respiratory syndrome coronavirus 2 (SARS-CoV-2, the virus that causes Covid-19), and the implication that the virus remains viable in aerosols is likely to influence infection-control practices. The authors used a three-jet Collison nebulizer to generate artificial particles that, because of their small size (<5 μm), remained suspended in aerosols. However, the authors did not provide data to support the choice of particle size or viral inoculum.",
-          author: [{
-              "familyName": "Rubens",
-              "givenName": "Jessica H",
-              "affiliation": "Johns Hopkins Hospital, Baltimore, MD sjain5@jhmi.edu."
-            },
-            {
-
-              "familyName": "Karakousis",
-              "givenName": "Petros C",
-              "Initials": "PC",
-              "affiliation": "Johns Hopkins Hospital, Baltimore, MD sjain5@jhmi.edu."
-            },
-            {
-
-              "familyName": "Jain",
-              "givenName": "Sanjay K",
-              "Initials": "SK",
-              "affiliation": "Johns Hopkins Hospital, Baltimore, MD sjain5@jhmi.edu."
-            }
-          ],
-          keywords: ["prevention"],
-          topicCategory: ["prevention"]
-
-        },
-        {
-          _id: "ihme",
-          "@type": "Analysis",
-          descriptionExpanded: false,
-          url: "https://covid19.healthdata.org/projections",
-          name: "COVID-19 U.S. State by State Projections",
-          author: [{
-            name: "Institute for Health Metrics and Evaluation",
-            affiliation: "University of Washington"
-          }],
-          mainEntity: "https://covid19.healthdata.org/",
-          dateCreated: "2020-03-30",
-          dateModified: "2020-03-31",
-          lastReviewed: "2020-04-01",
-          analysisTechnique: "Whole Genome Sequencing",
-          isPartOf: "http://www.healthdata.org/research-article/forecasting-covid-19-impact-hospital-bed-days-icu-days-ventilator-days-and-deaths",
-          isBasedOn: [{
-            "@type": "Dataset",
-            url: ""
-          }],
-          variableMeasured: "", // something similiar?
-          keywords: ["epidemiology", "outbreak size model"],
-          infectiousAgent: "SARS-CoV-2",
-          description: "The charts below show projected hospital resource use based on COVID-19 deaths. The model assumes continued social distancing until the end of May 2020. In locations without social distancing measures currently in place, we have assumed they will be in place within seven days of the last model update. If not, the number of deaths and burden on their hospital systems will likely be higher than the model predicts.",
-        },
-        {
-          _id: "jhu",
-          "@type": "Dataset",
-          descriptionExpanded: false,
-          url: "https://github.com/CSSEGISandData/COVID-19",
-          description: "This is the data repository for the 2019 Novel Coronavirus Visual Dashboard operated by the Johns Hopkins University Center for Systems Science and Engineering (JHU CSSE). Also, Supported by ESRI Living Atlas Team and the Johns Hopkins University Applied Physics Lab (JHU APL).",
-          name: "Novel Coronavirus (COVID-19) Cases",
-          keywords: ["epidemiology"],
-          author: [{
-            name: "Johns Hopkins University"
-          }],
-          dateCreated: "2020-01-22",
-          dateModified: "2020-04-01",
-          lastReviewed: "2020-04-01",
-        },
-        {
-          "@type": "ClinicalTrial",
-          "_id": "NCT04313322",
-          "identifier": "NCT04313322",
-          "identifierSource": "ClinicalTrials.gov",
-          "url": "https://clinicaltrials.gov/ct2/show/NCT04313322",
-          "name": "Treatment of COVID-19 Patients Using Wharton's Jelly-Mesenchymal Stem Cells",
-          "alternateName": ["Treatment of COVID-19 Patients Using Wharton's Jelly-Mesenchymal Stem Cells"],
-          "abstract": "The purpose of this study is to investigate the potential use of Wharton's Jelly Mesenchymal stem cells (WJ-MSCs) for treatment of patient diagnosed with Corona Virus SARS-CoV-2 infection, and showing symptoms of COVID-19.",
-          "description": "COVID-19 is a condition caused by infection with Coronoa Virus (SARS-CoV-2). This virus has a high transmission rate and is spreading at very high rates. causing a worldwide pandemic. Patients diagnosed with COVID-19 and confirmed positive with the virus, will be given three IV doses of WJ-MSCs consisting of 1X10e6\\/kg. The three doses will be 3 days apart form each other.\\n\\nPatients will be followed up for a period of three weeks to assess the severity of the condition and measure the viral titers.",
-          "sponsor": [{
-            "@type": "Organization",
-            "name": "Stem Cells Arabia",
-            "class": "other",
-            "role": "lead sponsor"
-          }],
-          "author": [{
-            "@type": "Person",
-            "name": "Adeeb Al Zoubi",
-            "affiliation": ["Stem Cells Arabia"],
-            "title": "President, Chief Scientific Officer",
-            "role": "Principal Investigator"
-          }, {
-            "@type": "Person",
-            "name": "Adeeb M AlZoubi, Ph.D.",
-            "role": "Contact"
-          }, {
-            "@type": "Person",
-            "name": "Ahmad Y AlGhadi, M.Sc.",
-            "role": "Contact"
-          }],
-          "studyStatus": {
-            "@type": "StudyStatus",
-            "status": "recruiting",
-            "statusDate": "March 2020",
-            "enrollmentCount": 5,
-            "enrollmentType": "anticipated"
-          },
-          "studyEvent": [{
-            "@type": "StudyEvent",
-            "studyEventType": "start",
-            "studyEventDate": "March 16, 2020",
-            "studyEventDateType": "anticipated"
-          }, {
-            "@type": "StudyEvent",
-            "studyEventType": "primary completion",
-            "studyEventDate": "June 30, 2020",
-            "studyEventDateType": "anticipated"
-          }, {
-            "@type": "StudyEvent",
-            "studyEventType": "completion",
-            "studyEventDate": "September 30, 2020",
-            "studyEventDateType": "anticipated"
-          }, {
-            "@type": "StudyEvent",
-            "studyEventType": "first posting to clinicaltrials.gov",
-            "studyEventDate": "March 18, 2020",
-            "studyEventDateType": "actual"
-          }, {
-            "@type": "StudyEvent",
-            "studyEventType": "last posting to clinicaltrials.gov",
-            "studyEventDate": "March 18, 2020",
-            "studyEventDateType": "actual"
-          }, {
-            "@type": "StudyEvent",
-            "studyEventType": "first submission",
-            "studyEventDate": "March 15, 2020"
-          }, {
-            "@type": "StudyEvent",
-            "studyEventType": "first submission that met quality control criteria",
-            "studyEventDate": "March 15, 2020"
-          }, {
-            "@type": "StudyEvent",
-            "studyEventType": "last update submission",
-            "studyEventDate": "March 15, 2020"
-          }],
-          "hasResults": false,
-          "dateCreated": "2020-03-15",
-          "datePublished": "2020-03-18",
-          "dateModified": "2020-03-18",
-          "curatedBy": {
-            "@type": "Organization",
-            "name": "ClinicalTrials.gov",
-            "url": "https://clinicaltrials.gov/ct2/results?cond=COVID-19",
-            "versionDate": "2020-04-29"
-          },
-          "healthCondition": ["Use of Stem Cells for COVID-19 Treatment"],
-          "keywords": ["Stem Cells", "COVID-19", "SARS CoV2", "WJ MSCs", "Immunomodulation"],
-          "studyDesign": {
-            "@type": "StudyDesign",
-            "studyType": "interventional",
-            "designModel": "single group assignment",
-            "designPrimaryPurpose": "treatment",
-            "phase": ["Phase 1"],
-            "phaseNumber": [1]
-          },
-          "outcome": [{
-            "@type": "Outcome",
-            "outcomeMeasure": "Clinical outcome",
-            "outcomeTimeFrame": "3 weeks",
-            "outcomeType": "primary"
-          }, {
-            "@type": "Outcome",
-            "outcomeMeasure": "CT Scan",
-            "outcomeTimeFrame": "3 weeks",
-            "outcomeType": "primary"
-          }, {
-            "@type": "Outcome",
-            "outcomeMeasure": "RT-PCR results",
-            "outcomeTimeFrame": "3 weeks",
-            "outcomeType": "primary"
-          }, {
-            "@type": "Outcome",
-            "outcomeMeasure": "RT-PCR results",
-            "outcomeTimeFrame": "8 weeks",
-            "outcomeType": "secondary"
-          }],
-          "eligibilityCriteria": [{
-            "@type": "Eligibility",
-            "inclusionCriteria": ["COVID-19 positive according to diagnosis and clinical management of COVID-19 criteria."],
-            "exclusionCriteria": ["Participants in other clinical trials", "patients with malignant tumors", "pregnant and lactating women", "co-infection with other infectious viruses or bacteria", "History of several allergies",
-              "Patients with history of pulmonary embolism", "any liver or kidney diseases", "HIV positive patients", "Considered by researchers to be not suitable to participate in this clinical trial",
-              "Chronic heart failure with ejection fraction less than 30%."
-            ],
-            "minimumAge": "18 years",
-            "gender": "all",
-            "healthyVolunteers": null,
-            "stdAge": ["adult", "older adult"]
-          }],
-          "isBasedOn": [],
-          "relatedTo": null,
-          "studyLocation": [{
-            "@type": "Place",
-            "name": "Stem Cells Arabia",
-            "studyLocationCity": "Amman",
-            "studyLocationCountry": "Jordan",
-            "studyLocationStatus": "recruiting"
-          }, {
-            "studyLocationCountry": "Australia",
-          }],
-          "armGroup": [{
-            "@type": "ArmGroup",
-            "name": "WJ-MSCs",
-            "description": "WJ-MSCs will be derived from cord tissue of newborns, screened for HIV1\\/2, HBV, HCV, CMV, Mycoplasma, and cultured to enrich for MSCs.\\n\\nWJ-MSCs will be counted and suspended in 25 ml of Saline solution containing 0.5% human serum Albumin, and will be given to patient intravenously.",
-            "role": "experimental",
-            "intervention": [{
-              "@type": "Intervention",
-              "category": "biological",
-              "name": "WJ-MSCs",
-              "description": "WJ-MSCs will be derived from cord tissue of newborns, screened for HIV1\\/2, HBV, HCV, CMV, Mycoplasma, and cultured to enrich for MSCs.\\n\\nWJ-MSCs will be counted and suspended in 25 ml of Saline solution containing 0.5% human serum Albumin, and will be given to patient intravenously."
-            }]
-          }]
-        },
-        {
-          _id: "protocols",
-          "@type": "Protocol",
-          url: "https://mammoth.bio/2020/02/15/white-paper-a-protocol-for-rapid-detection-of-sars-cov-2-using-crispr-sars-cov-2-detectr/",
-          curatedBy: {
-            url: "https://www.protocols.io/view/a-protocol-for-rapid-detection-of-the-2019-novel-c-bcmtiu6n",
-            name: "protocols.io"
-          },
-          name: "A protocol for rapid detection of the 2019 novel coronavirus SARS-CoV-2 using CRISPR diagnostics: SARS-CoV-2 DETECTR",
-          description: 'Given the global health emergency, rapid transmission, and severe respiratory disease associated with the outbreak of the 2019 novel coronavirus (SARS-CoV-2), Mammoth Biosciences has reconfigured our DETECTR platform to rapidly and accurately detect SARS-CoV-2 using a visual lateral flow strip format within 30 minutes from sample to result. To ensure specificity of detection, we selected a high-fidelity CRISPR detection enzyme and designed sets of gRNAs that can either 1) differentiate SARS-CoV-2 or 2) provide multi-coronavirus strain detection. SARS-CoV-2 DETECTR couples CRISPR detection with isothermal pre-amplification using primers based on protocols validated by the US Centers for Disease Control and Prevention (CDC) and World Health Organization (WHO). Currently in the United States, the CDC SARS-CoV-2 real-time RT-PCR diagnostic panel has a laboratory turnaround time of approximately 4-6 hours, with results that can be delayed for >24 hours after sample collection due to shipping requirements. In addition, these tests are only available in CDC-designated public health laboratories certified to perform high-complexity testing. Mammoth is working to enable point of care testing (POCT) solutions that can be deployed in areas at greatest risk of transmitting SARS-CoV-2 infection, including airports, emergency departments, and local community hospitals, particularly in low-resource countries. Leveraging an “off-the-shelf” strategy to enable practical solutions within a short time frame, we describe here a protocol that is fast (<30 min), practical (available immediately from international suppliers), and validated using contrived samples.',
-          measurementTechnique: "CRISPR",
-          identifier: "dx.doi.org/10.17504/protocols.io.bcmtiu6n",
-          author: [{
-              name: "Mammoth Biosciences"
-            },
-            {
-              name: "James P. Broughton"
-            },
-            {
-              name: "Wayne Deng"
-            },
-            {
-              name: "Clare L. Fasching"
-            },
-            {
-              name: "Jasmeet Singh"
-            },
-            {
-              name: "Charles Y. Chiu"
-            },
-            {
-              name: "Janice S. Chen"
-            }
-          ],
-          keywords: ["coronavirus", "SARS", "SARS-CoC-2", "CRISPR", "SARS-CoV-2 DETECTR"],
-          topicCategory: ["diagnostics"],
-          datePublished: "2020-02-17",
-          dateModified: "2020-03-17"
-        }
-
-      ]
     };
   }
 }
