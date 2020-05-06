cb9f29b73cdfc95aa77e6c00b14f11f0f4494906
Laura Hughes <flaneuse@users.noreply.github.com>
2020-05-05 17:58:15 -0700

    add access date

--- web/src/components/ResourceDescription.vue
+++ web/src/components/ResourceDescription.vue
@@ -1,205 +1,139 @@
 <template>
-  <div class="d-flex flex-column text-left" v-if="data">
-    <!-- authors -->
-    <div
-      class="author-container d-flex flex-wrap"
-      v-if="data.author || data.creator"
-    >
-      <template v-if="data.author">
-        <div
-          class="author"
-          v-for="(author, idx) in data.author"
-          :key="idx"
-          id="authors"
-        >
-          <span>{{
+<div class="d-flex flex-column text-left" v-if="data">
+  <!-- authors -->
+  <div class="author-container d-flex flex-wrap" v-if="data.author || data.creator">
+    <template v-if="data.author">
+      <div class="author" v-for="(author, idx) in data.author" :key="idx" id="authors">
+        <span>{{
             author.name
               ? author.name
               : author.givenName + " " + author.familyName
           }}</span>
-          <span v-if="idx < data.author.length - 2" v-html="',&nbsp;'"></span>
-          <span
-            v-if="idx == data.author.length - 2 && !data.author.length == 2"
-            v-html="',&nbsp;and&nbsp;'"
-          ></span>
-          <span
-            v-if="idx == data.author.length - 2 && data.author.length == 2"
-            v-html="'&nbsp;and&nbsp;'"
-          ></span>
-        </div>
+        <span v-if="idx < data.author.length - 2" v-html="',&nbsp;'"></span>
+        <span v-if="idx == data.author.length - 2 && !data.author.length == 2" v-html="',&nbsp;and&nbsp;'"></span>
+        <span v-if="idx == data.author.length - 2 && data.author.length == 2" v-html="'&nbsp;and&nbsp;'"></span>
+      </div>
 
-        <a @click.prevent="showAffiliation = !showAffiliation" href=""
-          ><small class="text-muted ml-2">
-            <span>{{
+      <a @click.prevent="showAffiliation = !showAffiliation" href=""><small class="text-muted ml-2">
+          <span>{{
               showAffiliation ? "hide affiliations" : "view affiliations"
             }}</span>
-            <i
-              class="fas fa-angle-double-down mx-1"
-              v-if="!showAffiliation"
-            ></i>
-            <i class="fas fa-angle-double-up mx-1" v-if="showAffiliation"></i>
-          </small>
-        </a>
+          <i class="fas fa-angle-double-down mx-1" v-if="!showAffiliation"></i>
+          <i class="fas fa-angle-double-up mx-1" v-if="showAffiliation"></i>
+        </small>
+      </a>
 
-        <div
-          id="author-affiliations"
-          class="d-flex flex-column w-100"
-          v-if="showAffiliation"
-        >
-          <small
-            v-for="(author, idx) in data.author"
-            :key="idx"
-            class="text-muted"
-          >
-            {{
+      <div id="author-affiliations" class="d-flex flex-column w-100" v-if="showAffiliation">
+        <small v-for="(author, idx) in data.author" :key="idx" class="text-muted">
+          <b>{{
               author.name
                 ? author.name
                 : author.givenName + " " + author.familyName
-            }}:
-            <template v-if="Array.isArray(data.author.affiliation)">
-              <span
-                v-for="(affiliation, idx) in author.affiliation"
-                :key="idx"
-                >{{ affiliation.name }}</span
-              >
-            </template>
-            <template v-else>
-              <span>{{ author.affiliation }}</span>
-            </template>
-          </small>
-        </div>
-      </template>
+            }}</b>:
+          <template v-if="Array.isArray(data.author.affiliation)">
+            <span v-for="(affiliation, idx) in author.affiliation" :key="idx">{{ affiliation.name }}</span>
+          </template>
+          <template v-else>
+            <span>{{ author.affiliation }}</span>
+          </template>
+        </small>
+      </div>
+    </template>
 
-      <template v-else-if="data.creator">
-        <div
-          class="creator"
-          v-for="(creator, idx) in data.creator"
-          :key="idx"
-          id="authors"
-        >
-          <span>{{
+    <template v-else-if="data.creator">
+      <div class="creator" v-for="(creator, idx) in data.creator" :key="idx" id="authors">
+        <span>{{
             creator.name
               ? creator.name
               : creator.givenName + " " + creator.familyName
           }}</span>
-          <span v-if="idx < data.creator.length - 2" v-html="',&nbsp;'"></span>
-          <span
-            v-if="idx == data.creator.length - 2 && !data.creator.length == 2"
-            v-html="',&nbsp;and&nbsp;'"
-          ></span>
-          <span
-            v-if="idx == data.creator.length - 2 && data.creator.length == 2"
-            v-html="'&nbsp;and&nbsp;'"
-          ></span>
-        </div>
+        <span v-if="idx < data.creator.length - 2" v-html="',&nbsp;'"></span>
+        <span v-if="idx == data.creator.length - 2 && !data.creator.length == 2" v-html="',&nbsp;and&nbsp;'"></span>
+        <span v-if="idx == data.creator.length - 2 && data.creator.length == 2" v-html="'&nbsp;and&nbsp;'"></span>
+      </div>
 
-        <a @click.prevent="showAffiliation = !showAffiliation" href=""
-          ><small class="text-muted ml-2">
-            <span>{{
+      <a @click.prevent="showAffiliation = !showAffiliation" href=""><small class="text-muted ml-2">
+          <span>{{
               showAffiliation ? "hide affiliations" : "view affiliations"
             }}</span>
-            <i
-              class="fas fa-angle-double-down mx-1"
-              v-if="!showAffiliation"
-            ></i>
-            <i class="fas fa-angle-double-up mx-1" v-if="showAffiliation"></i>
-          </small>
-        </a>
+          <i class="fas fa-angle-double-down mx-1" v-if="!showAffiliation"></i>
+          <i class="fas fa-angle-double-up mx-1" v-if="showAffiliation"></i>
+        </small>
+      </a>
 
-        <div
-          id="creator-affiliations"
-          class="d-flex flex-column w-100"
-          v-if="showAffiliation"
-        >
-          <small
-            v-for="(creator, idx) in data.creator"
-            :key="idx"
-            class="text-muted"
-          >
-            {{
+      <div id="creator-affiliations" class="d-flex flex-column w-100" v-if="showAffiliation">
+        <small v-for="(creator, idx) in data.creator" :key="idx" class="text-muted">
+          <b>{{
               creator.name
                 ? creator.name
                 : creator.givenName + " " + creator.familyName
-            }}:
-            <template v-if="Array.isArray(data.creator.affiliation)">
-              <span
-                v-for="(affiliation, idx) in creator.affiliation"
-                :key="idx"
-                >{{ affiliation.name }}</span
-              >
-            </template>
-            <template v-else>
-              <span>{{ creator.affiliation }}</span>
-            </template>
-          </small>
-        </div>
-      </template>
-    </div>
-    <!-- dates -->
-    <small
-      class="text-muted"
-      v-if="
+            }}</b>:
+          <template v-if="Array.isArray(data.creator.affiliation)">
+            <span v-for="(affiliation, idx) in creator.affiliation" :key="idx">{{ affiliation.name }}</span>
+          </template>
+          <template v-else>
+            <span>{{ creator.affiliation }}</span>
+          </template>
+        </small>
+      </div>
+    </template>
+  </div>
+  <!-- dates -->
+  <small class="text-muted" v-if="
         data.dateModified ||
           data.dateCreated ||
           data.dataUpdated ||
-          data.datePublished
-      "
-    >
-      <i class="far fa-clock"></i>
-      <span v-if="data.dateModified">
-        updated {{ this.formatDate(data.dateModified) }}
-      </span>
-      <span v-if="data.dateModified && data.datePublished">&bull;</span>
-      <span v-if="data.datePublished">
-        published {{ this.formatDate(data.datePublished) }}
-      </span>
-      <span
-        v-if="
-          (data.dateModified && data.dateCreated) ||
-            (data.datePublished && data.dateCreated)
-        "
-        >&bull;</span
-      >
-      <span v-if="data.dateCreated">
-        created {{ this.formatDate(data.dateCreated) }}
-      </span>
-    </small>
+          data.datePublished ||
+          data.curatedBy.versionDate
+      ">
+    <i class="far fa-clock"></i>
+    <span v-if="data.dateModified">
+      updated {{ this.formatDate(data.dateModified) }}
+    </span>
 
-    <!-- keywords -->
-    <div class="keyword-container flex flex-wrap mt-2">
-      <small
-        class="keyword px-2 py-1 my-1 mr-1"
-        v-for="(keyword, idx) in data.keywords"
-        :key="idx"
-        :data-tippy-info="`search ${keyword}`"
-      >
-        <router-link
-          :to="{
+    <span v-if="data.datePublished && data.dateModified
+        ">&bull;</span>
+    <span v-if="data.datePublished">
+      published {{ this.formatDate(data.datePublished) }}
+    </span>
+
+    <span v-if="data.dateCreated && (data.datePublished || data.dateModified)  ">&bull;</span>
+
+    <span v-if="data.dateCreated">
+      created {{ this.formatDate(data.dateCreated) }}
+    </span>
+
+    <span v-if="data.curatedBy && data.curatedBy.versionDate && (data.dateCreated || data.datePublished || data.dateModified)
+        ">&bull;</span>
+    <span v-if="data.curatedBy && data.curatedBy.versionDate">
+      accessed {{ this.formatDate(data.curatedBy.versionDate) }}
+    </span>
+  </small>
+
+  <!-- keywords -->
+  <div class="keyword-container flex flex-wrap mt-2">
+    <small class="keyword px-2 py-1 my-1 mr-1" v-for="(keyword, idx) in data.keywords" :key="idx" :data-tippy-info="`search ${keyword}`">
+      <router-link :to="{
             name: 'Resources',
             query: { search: `&quot;${keyword}&quot;` }
-          }"
-          class="no-underline text-dark"
-        >
-          {{ keyword }}
-        </router-link>
-      </small>
-    </div>
-    <!-- source -->
-    <div class="mt-2" v-if="data.curatedBy">
-      <small
-        >Record provided by
-        <a :href="data.curatedBy.url" target="_blank" rel="noreferrer"
-          >{{ data.curatedBy.name }}.</a
-        >
-        <router-link :to="{ name: 'Sources' }"> Learn more</router-link>
-      </small>
-    </div>
-    <!-- description -->
-    <div class="mt-4" id="description">
-      <div v-html="data.abstract" v-if="data.abstract"></div>
-      <div v-html="data.description" v-else></div>
-    </div>
+          }" class="no-underline text-dark">
+        {{ keyword }}
+      </router-link>
+    </small>
   </div>
+  <!-- source -->
+  <div class="mt-2" v-if="data.curatedBy">
+    <small>Record provided by
+      <a :href="data.curatedBy.url" target="_blank" rel="noreferrer">{{ data.curatedBy.name }}.</a>
+      <router-link :to="{ name: 'Sources' }"> Learn more</router-link>
+    </small>
+  </div>
+  <!-- description -->
+  <div class="mt-4" id="description">
+    <div v-html="data.abstract" v-if="data.abstract"></div>
+    <div v-html="data.description" v-else></div>
+  </div>
+</div>
 </template>
 
 <script lang="js">
@@ -268,30 +202,30 @@ export default Vue.extend({
 
 <style lang="scss" scoped>
 .resource-type {
-  font-weight: 700;
-  text-transform: uppercase;
-  opacity: 0.7;
+    font-weight: 700;
+    text-transform: uppercase;
+    opacity: 0.7;
 }
 
 .pub-type {
-  opacity: 0.6;
+    opacity: 0.6;
 }
 
 .keyword {
-  background: lighten($warning-color, 35%);
-  border-radius: 5px;
-  white-space: nowrap;
+    background: lighten($warning-color, 35%);
+    border-radius: 5px;
+    white-space: nowrap;
 }
 
 .pub-type {
-  opacity: 0.6;
+    opacity: 0.6;
 }
 
 .helper {
-  line-height: 1.2em;
+    line-height: 1.2em;
 }
 
 .section-header {
-  text-transform: uppercase;
+    text-transform: uppercase;
 }
 </style>
