b07e3249950ac58b6e088dc8961ccefc0e1d9ff8
Laura Hughes <flaneuse@users.noreply.github.com>
2020-05-11 11:05:14 -0700

    add backup intervention text

--- web/src/components/ClinicalTrialDescription.vue
+++ web/src/components/ClinicalTrialDescription.vue
@@ -1,61 +1,63 @@
 <template>
-  <div class="mr-5">
-    <!-- design -->
-    <div id="design" class="text-left border-top border-bottom text-muted py-3 my-3">
-      <h6 class="m-0">Study Design</h6>
-      <div v-if="data.studyDesign">
-        <TrialDesign :data="data" />
-      </div>
-      <div v-else>
-        <small>not specified</small>
-      </div>
+<div class="mr-5">
+  <!-- design -->
+  <div id="design" class="text-left border-top border-bottom text-muted py-3 my-3">
+    <h6 class="m-0">Study Design</h6>
+    <div v-if="data.studyDesign">
+      <TrialDesign :data="data" />
     </div>
-
-    <!-- interventions -->
-    <div id="interventions" class="text-left border-bottom text-muted pb-3 mb-3">
-      <h6 class="m-0">Interventions</h6>
-      <div v-if="data.armGroups || data.interventions">
-        <TrialInterventions :data="data" />
-      </div>
-      <div v-else>
-        <small>not specified</small>
-      </div>
+    <div v-else>
+      <small>not specified</small>
     </div>
-
-    <!-- eligibility -->
-    <div id="eligibility" class="text-left border-bottom text-muted pb-3 mb-3">
-      <h6 class="m-0">Eligibility</h6>
-      <div v-if="data.eligibilityCriteria">
-        <TrialEligibility :data="data.eligibilityCriteria" />
-      </div>
-      <div v-else>
-        <small>not specified</small>
-      </div>
-    </div>
-
-    <!-- outcome -->
-    <div id="outcome" class="text-left border-bottom text-muted pb-3 mb-3">
-      <h6 class="m-0">Outcome</h6>
-      <div v-if="data.outcome">
-        <TrialOutcome :data="data.outcome" />
-      </div>
-      <div v-else>
-        <small>not specified</small>
-      </div>
-    </div>
-
-    <!-- status -->
-    <div id="status" class="text-left border-bottom text-muted pb-3 mb-3">
-      <h6 class="m-0">Status</h6>
-      <div v-if="data.studyStatus || data.studyEvent">
-        <TrialEvents :data="data" />
-      </div>
-      <div v-else>
-        <small>not specified</small>
-      </div>
-    </div>
-
   </div>
+
+  <!-- interventions -->
+  <div id="interventions" class="text-left border-bottom text-muted pb-3 mb-3">
+    <h6 class="m-0">Interventions</h6>
+    <div v-if="data.armGroup || data.interventions">
+      <TrialInterventions :data="data" />
+    </div>
+    <div v-else-if="data.interventionText">
+      {{data.interventionText}}</div>
+    <div v-else>
+      <small>not specified</small>
+    </div>
+  </div>
+
+  <!-- eligibility -->
+  <div id="eligibility" class="text-left border-bottom text-muted pb-3 mb-3">
+    <h6 class="m-0">Eligibility</h6>
+    <div v-if="data.eligibilityCriteria">
+      <TrialEligibility :data="data.eligibilityCriteria" />
+    </div>
+    <div v-else>
+      <small>not specified</small>
+    </div>
+  </div>
+
+  <!-- outcome -->
+  <div id="outcome" class="text-left border-bottom text-muted pb-3 mb-3">
+    <h6 class="m-0">Outcome</h6>
+    <div v-if="data.outcome">
+      <TrialOutcome :data="data.outcome" />
+    </div>
+    <div v-else>
+      <small>not specified</small>
+    </div>
+  </div>
+
+  <!-- status -->
+  <div id="status" class="text-left border-bottom text-muted pb-3 mb-3">
+    <h6 class="m-0">Status</h6>
+    <div v-if="data.studyStatus || data.studyEvent">
+      <TrialEvents :data="data" />
+    </div>
+    <div v-else>
+      <small>not specified</small>
+    </div>
+  </div>
+
+</div>
 </template>
 
 
