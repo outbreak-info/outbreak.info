1a5d68d14110fb604714952f18fb9d12cfedf572
Laura Hughes <flaneuse@users.noreply.github.com>
2020-06-08 17:23:07 -0700

    Update DataUsage.vue

--- web/src/components/DataUsage.vue
+++ web/src/components/DataUsage.vue
@@ -1,9 +1,20 @@
 <template>
-  <div>
-<h6 class="m-0">Data usage policy</h6>
-<p>
-  outbreak.info combines together data and metadata from a large number of 
-</p>
+<div>
+  <h5 class="m-0">Data usage policy</h5>
+  <p>
+    outbreak.info combines together data and metadata from a large number of <router-link :to="{name: 'Sources'}">data sources</router-link>, and we abide by their data usage licenses.
+  </p>
 
-  </div>
+  <p>
+    You are welcome to copy, distribute, and develop data, visualizations, and website content from outbreak.info for any non-commercial uses, including any personal, editorial, academic, or research purposes.
+  </p>
+
+  <p>
+    outbreak.info and website content is published under a <a href="https://creativecommons.org/licenses/by-nc/4.0/" target = "_blank" rel="noreferrer">Creative Commons CC BY-NC-4.0 license</a>, which requires users to
+    attribute the source and license type (CC BY-NC-4.0) when sharing our data or website content, as specified below. All commercial uses are not permitted under the Creative Commons license unless allowed by the specific data provider(s).
+  </p>
+
+  <small>Data usage policy adapted from <a href="https://covidtracking.com/license" target="_blank" rel="noreferrer">COVID Tracking Project</a></small>
+
+</div>
 </template>
