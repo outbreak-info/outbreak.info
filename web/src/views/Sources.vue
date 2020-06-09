66ea072d8f84db06e9703611000e026a2df961ab
Laura Hughes <flaneuse@users.noreply.github.com>
2020-06-09 09:22:16 -0700

    create reusable SourceDescription, add license/citation

--- web/src/views/Sources.vue
+++ web/src/views/Sources.vue
@@ -1,96 +1,50 @@
 <template>
-  <div>
-    <div class="row m-0">
-      <div
-        class="col-sm-12 bg-light d-flex justify-content-center align-items-center my-5"
-      >
-        <div class="container half-page">
-          <div class="d-flex align-items-center justify-content-between mb-5">
-            <h1>Data Sources</h1>
-            <router-link class="btn btn-main" :to="{ name: 'Contributing' }"
-              >Contribute data</router-link
-            >
-          </div>
+<div>
+  <div class="row m-0">
+    <div class="col-sm-12 bg-light d-flex justify-content-center align-items-center my-5">
+      <div class="container half-page">
+        <div class="d-flex align-items-center justify-content-between mb-5">
+          <h1>Data Sources</h1>
+          <router-link class="btn btn-main" :to="{ name: 'Contributing' }">Contribute data</router-link>
+        </div>
 
-          <div class="text-left">
-            <h3 id="epidemiology">Epidemiology Data</h3>
-            <div
-              class="epi-container"
-              v-for="source in sources"
-              :key="source.id"
-            >
-            <h5 class="m-0 mb-1">
-              <a :href="source.url" target="_blank" rel="noreferrer"  class="d-flex align-items-center justify-content-between">{{
-                source.name
-              }}
-              <img class="ml-3" :src="require(`@/assets/resources/${source.img_lg}`)" :alt="source.name" height = "32" v-if="source.img_lg" />
-              <img class="ml-3" :src="require(`@/assets/resources/${source.img}`)" :alt="source.name" height = "32" v-else-if="source.img" />
-            </a>
-            </h5>
-              <p v-html="source.description" class="text-justify mb-4"></p>
-            </div>
-          </div>
+        <div class="text-left">
+          <h3 id="epidemiology">Epidemiology Data</h3>
+          <SourceDescription :sources="sources" />
+        </div>
 
-          <div class="text-left mt-5">
-            <h3 id="geographic">Geographic Data</h3>
-            <div
-              class="epi-container"
-              v-for="source in geoSources"
-              :key="source.id"
-            >
-            <h5 class="m-0 mb-1">
-              <a :href="source.url" target="_blank" rel="noreferrer"  class="d-flex align-items-center justify-content-between">{{
-                source.name
-              }}
-              <img class="ml-3" :src="require(`@/assets/resources/${source.img_lg}`)" :alt="source.name" height = "32" v-if="source.img_lg" />
-              <img class="ml-3" :src="require(`@/assets/resources/${source.img}`)" :alt="source.name" height = "32" v-else-if="source.img" />
-            </a>
-            </h5>
-              <p class="text-justify mb-4">
-                {{ source.description }}
-              </p>
-            </div>
-          </div>
+        <div class="text-left mt-5">
+          <h3 id="geographic">Geographic Data</h3>
+          <SourceDescription :sources="geoSources" />
+        </div>
 
-          <div class="text-left mt-5">
-            <h3 id="resources" class="mb-3 border-top pt-4">Resources</h3>
-            <div
-              v-for="(resource, idx) in resources"
-              :key="idx"
-              :class="[idx === 0 ? 'mb-5' : 'my-5']"
-            >
-              <h4>{{ resource.category }}</h4>
-              <div
-                class="resources-container"
-                v-for="source in resource.sources"
-                :key="source.id"
-              >
-                <h5 class="m-0 mb-1">
-                  <a :href="source.url" target="_blank" rel="noreferrer"  class="d-flex align-items-center justify-content-between">{{
-                    source.name
-                  }}
-                  <img class="ml-3" :src="require(`@/assets/resources/${source.img_lg}`)" :alt="source.name" height = "32" v-if="source.img_lg" />
-                  <img class="ml-3" :src="require(`@/assets/resources/${source.img}`)" :alt="source.name" height = "32" v-else-if="source.img" />
-                </a>
-                </h5>
-                <p class="text-justify mb-4" v-html="source.description"></p>
-              </div>
-            </div>
+        <div class="text-left mt-5">
+          <h3 id="resources" class="mb-3 border-top pt-4">Resources</h3>
+          <div v-for="(resource, idx) in resources" :key="idx" :class="[idx === 0 ? 'mb-5' : 'my-5']">
+            <h4>{{ resource.category }}</h4>
+            <SourceDescription :sources="resource.sources" />
           </div>
         </div>
       </div>
     </div>
   </div>
+</div>
 </template>
 
-<script lang="ts">
+<script lang="js">
 import Vue from "vue";
 
-import { mapState } from "vuex";
+import {
+  mapState
+} from "vuex";
+
+import SourceDescription from "@/components/SourceDescription.vue";
 
 export default Vue.extend({
   name: "Sources",
-  components: {},
+  components: {
+    SourceDescription
+  },
   computed: {
     ...mapState("admin", ["sources", "geoSources", "resources"])
   }
