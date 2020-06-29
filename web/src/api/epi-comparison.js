f200ac6ccc74ccc3807563f7bd06724e985c8b97
Laura Hughes <flaneuse@users.noreply.github.com>
2020-06-29 11:48:55 -0700

    pull today's date dynamically

--- web/src/api/epi-comparison.js
+++ web/src/api/epi-comparison.js
@@ -14,7 +14,9 @@ import {
 import {
   nest,
   timeParse,
-  format
+  timeDay,
+  offset,
+  timeFormat
 } from "d3";
 
 import {
@@ -28,63 +30,76 @@ export function getComparisonData(apiUrl, location, adminLevel, sort, page, size
   const queryString = location ? `${location} AND admin_level:("${adminLevel}")` : `admin_level:${adminLevel}`;
 
   return getCurrentData(apiUrl, queryString, sort, page, size)
-  // return getAll(apiUrl, queryString)
+    // return getAll(apiUrl, queryString)
     .pipe(
       map(results => {
-        return(results)
+        return (results)
       }),
-    catchError(e => {
-      console.log("%c Error in getting comparison!", "color: red");
-      console.log(e);
-      return from([]);
-    }),
-    finalize(() => (store.state.admin.loading = false))
-  )
+      catchError(e => {
+        console.log("%c Error in getting comparison!", "color: red");
+        console.log(e);
+        return from([]);
+      }),
+      finalize(() => (store.state.admin.loading = false))
+    )
 }
 
 export function getCurrentData(apiUrl, queryString, sort, page, size) {
   const parseDate = timeParse("%Y-%m-%d");
+  const formatDate = timeFormat("%Y-%m-%d");
 
   const timestamp = Math.round(new Date().getTime() / 1e5);
-  const fields = "date,location_id,name,state_name,confirmed,confirmed_numIncrease,confirmed_pctIncrease,confirmed_rolling,dead,dead_numIncrease,dead_pctIncrease,dead_rolling"
+  const fields = "date,location_id,name,state_name,confirmed,confirmed_numIncrease,confirmed_pctIncrease,confirmed_rolling,dead,dead_numIncrease,dead_pctIncrease,dead_rolling";
 
-  const qString = `(date:"2020-06-24" OR date:"2020-06-10") AND (${queryString})&sort=${"-date"}&size=${size}&from=${page}&fields=${fields}`;
-  return getAll(apiUrl, qString)
-    .pipe(
-      map(results => {
-        results.forEach(result => {
-          result["datetime"] = parseDate(result.date);
-        })
+  return from(
+    axios.get(
+      `${apiUrl}query?q=mostRecent:true&fields=date&size=1&sort=-date&timestamp=${timestamp}`
+    )
+  ).pipe(
+    pluck("data", "hits"),
+    mergeMap(maxDate => {
+      const currentDate = parseDate(maxDate[0].date);
+      const twoWeeks = timeDay.offset(currentDate, -14);
 
-        results.sort((a,b) => b.datetime < a.datetime ? -1 : 1);
+      const qString = `(date:"${maxDate[0].date}" OR date:"${formatDate(twoWeeks)}") AND (${queryString})&sort=${"-date"}&size=${size}&from=${page}&fields=${fields}`;
 
-        const nested = nest()
-        .key(d => d.location_id)
-        .rollup(values => {
-          return({
-          location_id: values[0].location_id,
-          all: values,
-          name: values[0].name,
-          date: values[0].date,
-          datetime: values[0].datetime,
-          dead: values[0].dead,
-          dead_numIncrease: values[0].dead_numIncrease,
-          dead_rolling: values[0].dead_rolling,
-          confirmed: values[0].confirmed,
-          confirmed_numIncrease: values[0].confirmed_numIncrease,
-          confirmed_rolling: values[0].confirmed_rolling,
-          confirmed_change: values.length == 2 ? values[0].confirmed_rolling - values[1].confirmed_rolling : null,
-          dead_change: values.length == 2 ? values[0].dead_rolling - values[1].dead_rolling : null
+      return getAll(apiUrl, qString)
+        .pipe(
+          map(results => {
+            results.forEach(result => {
+              result["datetime"] = parseDate(result.date);
+            })
+
+            results.sort((a, b) => b.datetime < a.datetime ? -1 : 1);
+
+            const nested = nest()
+              .key(d => d.location_id)
+              .rollup(values => {
+                return ({
+                  location_id: values[0].location_id,
+                  all: values,
+                  name: values[0].name,
+                  date: values[0].date,
+                  datetime: values[0].datetime,
+                  dead: values[0].dead,
+                  dead_numIncrease: values[0].dead_numIncrease,
+                  dead_rolling: values[0].dead_rolling,
+                  confirmed: values[0].confirmed,
+                  confirmed_numIncrease: values[0].confirmed_numIncrease,
+                  confirmed_rolling: values[0].confirmed_rolling,
+                  confirmed_change: values.length == 2 ? values[0].confirmed_rolling - values[1].confirmed_rolling : null,
+                  dead_change: values.length == 2 ? values[0].dead_rolling - values[1].dead_rolling : null
+                })
+              })
+              .entries(results).map(d => d.value);
+
+            return (nested)
+          }),
+          catchError(e => {
+            console.log("%c Error in getting current data!", "color: red");
+            console.log(e);
+            return from([]);
           })
-        })
-        .entries(results).map(d => d.value);
-
-        return(nested)
-      }),
-    catchError(e => {
-      console.log("%c Error in getting current data!", "color: red");
-      console.log(e);
-      return from([]);
-    })
-  )
+        )
+    }))
 }
