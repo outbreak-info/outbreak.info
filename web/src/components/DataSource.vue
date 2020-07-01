0443a8a0ef4939bb751f7e4fcfde56b56ea67559
Laura Hughes <flaneuse@users.noreply.github.com>
2020-07-01 10:22:25 -0700

    init WIP

--- web/src/components/DataSource.vue
+++ web/src/components/DataSource.vue
@@ -13,6 +13,7 @@
 
     <!-- date updated -->
     <DataUpdated />
+    <i class="far fa-copy btn ml-3 btn-main-outline" @click="copyPng"></i>
     <DownloadData class="ml-3" id="download-btn" v-if="data" :type="dataType" :figureRef="figureRef" :data="data" :sourceString="sourceString" />
 
   </div>
@@ -22,6 +23,7 @@
 import Vue from "vue";
 
 import { mapState } from "vuex";
+import { getPng } from "@/js/get_png.js";
 import DownloadData from "@/components/DownloadData.vue";
 import DataUpdated from "@/components/DataUpdated.vue";
 
@@ -54,7 +56,13 @@ export default Vue.extend({
     return {};
   },
   watch: {},
-  methods: {}
+  methods: {
+    copyPng(){
+      console.log("png");
+    const x =  getPng("epi-map-svg");
+    console.log(x)
+    }
+  }
 });
 </script>
 
---
+++ web/src/js/get_png.js
@@ -0,0 +1,73 @@
+// based on https://github.com/mbostock/svjimmy/blob/master/index.js
+//
+export function getPng(classID, download = false) {
+  console.log("getting your png")
+  // const refs = document.getElementsByClassName(classID);
+  var document = global.document,
+    body = document.body,
+    forEach = Array.prototype.forEach,
+    styles = document.querySelectorAll("style");
+  // console.log(refs)
+  // var emptySvg = window.document.createElementNS(this.prefix.svg, 'svg');
+  // window.document.body.appendChild(emptySvg);
+  // var emptySvgDeclarationComputed = getComputedStyle(emptySvg);
+
+  // const svgObject = this.getSvgSources(refs, emptySvgDeclarationComputed);
+  // const filenames = svgObject.map(svg => this.filename + "_" + svg.name + ".svg").join(", ");
+  forEach.call(document.querySelectorAll("svg.epi-bargraph"), function(svg) {
+    console.log(svg)
+    if (svg.namespaceURI !== "http://www.w3.org/2000/svg") return; // Not really an SVG.
+    if (svg.ownerSVGElement) return; // An SVG within another SVG.
+
+    forEach.call(styles, function(style) {
+      svg.appendChild(style.cloneNode(true));
+    });
+
+    var canvas = document.createElement("canvas"),
+      context = canvas.getContext("2d"),
+      image = new Image,
+      ratio = global.devicePixelRatio || 1,
+      rect = svg.getBoundingClientRect(),
+      width = rect.width * ratio,
+      height = rect.height * ratio,
+      imageUrl = URL.createObjectURL(new Blob([(new XMLSerializer).serializeToString(svg)], {
+        type: "image/svg+xml"
+      }));
+
+    image.onload = function() {
+      setTimeout(function() {
+        context.drawImage(image, 0, 0, width, height);
+
+        var data = [new ClipboardItem({ "text/plain": new Blob(["Text data"], { type: "text/plain" }) })];
+navigator.clipboard.write(data).then(function() {
+  console.log("Copied to clipboard successfully!");
+}, function() {
+  console.error("Unable to write to clipboard. :-(");
+});
+
+   // canvas.toBlob(blob => navigator.clipboard.write([new ClipboardItem({'image/png': blob})]));
+
+        if (download) {
+          canvas.toBlob(function(blob) {
+            var a = document.createElement("a"),
+              aUrl = URL.createObjectURL(blob);
+            a.download = "untitled.png";
+            a.href = aUrl;
+            body.appendChild(a);
+            setTimeout(function() {
+              a.click();
+              aUrl = URL.revokeObjectURL(aUrl);
+              imageUrl = URL.revokeObjectURL(imageUrl);
+              body.removeChild(a);
+            }, 10);
+          });
+        }
+      }, 10);
+    };
+
+    canvas.width = width;
+    canvas.height = height;
+    image.src = imageUrl;
+  });
+
+}
