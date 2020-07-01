4680256e8f00c5b322ba37b7825f334abadbfef2
Laura Hughes <flaneuse@users.noreply.github.com>
2020-07-01 12:30:25 -0700

    return Promise from img copying

--- web/src/components/DataSource.vue
+++ web/src/components/DataSource.vue
@@ -1,33 +1,33 @@
 <template>
-  <div class="source my-3 d-flex align-items-center">
-    <small
-      >Source:
-      <span v-for="(source, idx) in filteredSources" :key="idx">
-        <a :href="source.url" target="_blank" rel="noreferrer"
-          >{{ source.name }} <span v-if="source.scope">({{ source.scope }})</span></a
-        >
-        <span v-if="idx < filteredSources.length - 1">; </span> </span
-      >, updated daily.
-      <router-link :to="{ name: 'Sources' }" class="mx-2">Read more</router-link>
-    </small>
+<div class="source my-3 d-flex align-items-center">
+  <small>Source:
+    <span v-for="(source, idx) in filteredSources" :key="idx">
+      <a :href="source.url" target="_blank" rel="noreferrer">{{ source.name }} <span v-if="source.scope">({{ source.scope }})</span></a>
+      <span v-if="idx < filteredSources.length - 1">; </span> </span>, updated daily.
+    <router-link :to="{ name: 'Sources' }" class="mx-2">Read more</router-link>
+  </small>
 
-    <!-- date updated -->
-    <DataUpdated />
-    <i class="far fa-copy btn ml-3 btn-main-outline" @click="copyPng"></i>
-    <DownloadData class="ml-3" id="download-btn" v-if="data" :type="dataType" :figureRef="figureRef" :data="data" :sourceString="sourceString" />
+  <!-- date updated -->
+  <DataUpdated />
+  <i class="far fa-copy btn ml-3 btn-main-outline" @click="copyPng"></i>
+  <DownloadData class="ml-3" id="download-btn" v-if="data" :type="dataType" :figureRef="figureRef" :data="data" :sourceString="sourceString" />
 
-    <p :class="{ snackbar: true, show: showSnackbar }">
-      {{ snackbarText }}
-    </p>
+  <p :class="{ snackbar: true, show: showSnackbar }">
+    {{ snackbarText }}
+  </p>
 
-  </div>
+</div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 
-import { mapState } from "vuex";
-import { getPng } from "@/js/get_png.js";
+import {
+  mapState
+} from "vuex";
+import {
+  getPng
+} from "@/js/get_png.js";
 import DownloadData from "@/components/DownloadData.vue";
 import DataUpdated from "@/components/DataUpdated.vue";
 
@@ -53,20 +53,32 @@ export default Vue.extend({
       }
     },
     sourceString() {
-      return(this.filteredSources.map(d => d.scope ? `${d.name} (${d.scope})` : `${d.name}`).join("; ") + ", updated daily")
+      return (this.filteredSources.map(d => d.scope ? `${d.name} (${d.scope})` : `${d.name}`).join("; ") + ", updated daily")
     }
   },
   data() {
     return {
-      showSnackbar: true,
+      showSnackbar: false,
       snackbarText: "copying figure to the clipboard"
     };
   },
   watch: {},
   methods: {
-    copyPng(){
-
-    getPng("svg.epi-bargraph");
+    copyPng() {
+      this.showSnackbar = true;
+      this.snackbarText = "copying figure to the clipboard";
+      getPng(`svg.${this.figureRef}`).then(msg => {
+        this.snackbarText = msg;
+        setTimeout(() => {
+          this.showSnackbar = false;
+        }, 3000);
+      }).catch(error => {
+        this.snackbarText = "Erorr copying image";
+        setTimeout(() => {
+          this.showSnackbar = false;
+        }, 3000);
+        console.log("Error: no svg found with that selector")
+      });
     }
   }
 });
--- web/src/js/get_png.js
+++ web/src/js/get_png.js
@@ -2,80 +2,81 @@
 //
 
 export function getPng(selector, download = false) {
-  // const refs = document.getElementsByClassName(classID);
-  var document = global.document,
-    body = document.body,
-    forEach = Array.prototype.forEach,
-    styles = document.querySelectorAll("style");
-  // console.log(refs)
-  // var emptySvg = window.document.createElementNS(this.prefix.svg, 'svg');
-  // window.document.body.appendChild(emptySvg);
-  // var emptySvgDeclarationComputed = getComputedStyle(emptySvg);
+  return new Promise((resolve, reject) => {
+    // const refs = document.getElementsByClassName(classID);
+    var document = global.document,
+      body = document.body,
+      forEach = Array.prototype.forEach,
+      styles = document.querySelectorAll("style");
 
-  // const svgObject = this.getSvgSources(refs, emptySvgDeclarationComputed);
-  // const filenames = svgObject.map(svg => this.filename + "_" + svg.name + ".svg").join(", ");
-  forEach.call(document.querySelectorAll(selector), function(svg) {
-    if (svg.namespaceURI !== "http://www.w3.org/2000/svg") return; // Not really an SVG.
-    if (svg.ownerSVGElement) return; // An SVG within another SVG.
+    const svgs = document.querySelectorAll(selector);
+    if (svgs.length === 0) {
+      reject("Error: no svg found with that selector")
+    }
 
-    forEach.call(styles, function(style) {
-      svg.appendChild(style.cloneNode(true));
-    });
+    forEach.call(svgs, function(svg) {
+      if (svg.namespaceURI !== "http://www.w3.org/2000/svg") return; // Not really an SVG.
+      if (svg.ownerSVGElement) return; // An SVG within another SVG.
 
-    var canvas = document.createElement("canvas"),
-      context = canvas.getContext("2d"),
-      image = new Image,
-      ratio = global.devicePixelRatio || 1,
-      rect = svg.getBoundingClientRect(),
-      width = rect.width * ratio,
-      height = rect.height * ratio,
-      imageUrl = URL.createObjectURL(new Blob([(new XMLSerializer).serializeToString(svg)], {
-        type: "image/svg+xml"
-      }));
+      forEach.call(styles, function(style) {
+        svg.appendChild(style.cloneNode(true));
+      });
 
-    image.onload = function() {
-      setTimeout(function() {
-        context.drawImage(image, 0, 0, width, height);
+      var canvas = document.createElement("canvas"),
+        context = canvas.getContext("2d"),
+        image = new Image,
+        ratio = global.devicePixelRatio || 1,
+        rect = svg.getBoundingClientRect(),
+        width = rect.width * ratio,
+        height = rect.height * ratio,
+        imageUrl = URL.createObjectURL(new Blob([(new XMLSerializer).serializeToString(svg)], {
+          type: "image/svg+xml"
+        }));
 
-        if (download) {
-          canvas.toBlob(function(blob) {
-            var a = document.createElement("a"),
-              aUrl = URL.createObjectURL(blob);
-            a.download = "untitled.png";
-            a.href = aUrl;
-            body.appendChild(a);
-            setTimeout(function() {
-              a.click();
-              aUrl = URL.revokeObjectURL(aUrl);
-              imageUrl = URL.revokeObjectURL(imageUrl);
-              body.removeChild(a);
-            }, 10);
-          });
-        }
-        // copy
-        else {
-          if (navigator.clipboard) {
-            console.log("clipping")
-            canvas.toBlob(blob => {
-              var data = [new ClipboardItem({"image/png": blob})];
+      image.onload = function() {
+        setTimeout(function() {
+          context.drawImage(image, 0, 0, width, height);
 
-              navigator.clipboard.write(data).then(function() {
-                console.log("Copied to clipboard successfully!");
-                return("copied to the clipboard")
-              }, function() {
-                console.error("Unable to write to clipboard. :-(");
-                return("sorry; copying this figure is unavailable")
-              });
-            })
+          if (download) {
+            canvas.toBlob(function(blob) {
+              var a = document.createElement("a"),
+                aUrl = URL.createObjectURL(blob);
+              a.download = "untitled.png";
+              a.href = aUrl;
+              body.appendChild(a);
+              setTimeout(function() {
+                a.click();
+                aUrl = URL.revokeObjectURL(aUrl);
+                imageUrl = URL.revokeObjectURL(imageUrl);
+                body.removeChild(a);
+              }, 10);
+            });
           }
+          // copy
+          else {
+            if (navigator.clipboard) {
+              canvas.toBlob(blob => {
+                var data = [new ClipboardItem({
+                  "image/png": blob
+                })];
 
-        }
-      }, 10);
-    };
+                navigator.clipboard.write(data).then(function() {
+                  resolve("copied to the clipboard")
+                }, function() {
+                  console.error("Unable to write to clipboard. :-(");
+                  resolve("sorry; copying this figure is unavailable")
+                });
+              })
+            }
 
-    canvas.width = width;
-    canvas.height = height;
-    image.src = imageUrl;
-  });
+          }
+        }, 10);
+      };
 
+      canvas.width = width;
+      canvas.height = height;
+      image.src = imageUrl;
+    });
+    // reject("ERROR: no svg found with that selector")
+  })
 }
