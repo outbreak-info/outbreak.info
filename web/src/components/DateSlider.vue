1e152a9284f4b1832e6230bd7ee40a6c9f3cec03
Laura Hughes <flaneuse@users.noreply.github.com>
2020-08-06 16:46:56 -0700

    whoops; only send GA event on play
    not play and pause

--- web/src/components/DateSlider.vue
+++ web/src/components/DateSlider.vue
@@ -92,11 +92,6 @@ export default Vue.extend({
   },
   methods: {
     play() {
-      this.$gtag.event("map_play", {
-        'event_category': `map_play`,
-        'event_label': `playing map starting from [${this.$route.fullPath}])`
-      })
-
       this.isPlaying = !this.isPlaying;
 
       const dayGap = 3; // parameter for how many days between
@@ -106,6 +101,11 @@ export default Vue.extend({
       }
 
       if (this.isPlaying) {
+        this.$gtag.event("map_play", {
+          'event_category': `map_play`,
+          'event_label': `playing map starting from [${this.$route.fullPath}])`
+        });
+        
         this.start(dayGap);
       }
     },
