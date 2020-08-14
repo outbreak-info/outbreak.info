74d3a857a1425b4b11cfd6627e4f1fb80da4100d
Laura Hughes <flaneuse@users.noreply.github.com>
2020-08-14 15:25:23 -0700

    only return top 5 closest results

--- web/src/api/find-similar.js
+++ web/src/api/find-similar.js
@@ -18,14 +18,14 @@ import {
 } from "@/api/biothings.js";
 
 
-export function findSimilar(apiUrl, locationID, variable, similarityMetric) {
+export function findSimilar(apiUrl, locationID, variable, similarityMetric, num2Return = 5) {
   store.state.admin.loading = true;
   // Choosing one specific date, since all dates contain the current info.
   // First get the location's data for the most recent date.
   // Use that value to get the most recent value of `similarityMetric` and find locations with similar values
   // Then get ALL the data for all those locations.
   return getLocation(apiUrl, locationID, variable, similarityMetric).pipe(
-    mergeMap(locationData => getSimilarData(apiUrl, locationData, similarityMetric, true).pipe(
+    mergeMap(locationData => getSimilarData(apiUrl, locationData, similarityMetric, num2Return, true).pipe(
       mergeMap(similar => {
         const locationString = `(${similar.map(d => d.location_id).join(" OR ")})`;
         return (getLocation(apiUrl, locationString, variable, similarityMetric)).pipe(
@@ -89,15 +89,15 @@ export function getLocation(apiUrl, locationID, variable, similarityMetric, most
       console.log("%c Error in getting map data!", "color: red");
       console.log(e);
       return from([]);
-    }),
-    finalize(() => (store.state.admin.loading = false))
+    })
   );
 }
 
-export function getSimilarData(apiUrl, locationData, similarityMetric, logged = true) {
+export function getSimilarData(apiUrl, locationData, similarityMetric, num2Return, logged = true) {
   const threshold = 0.02;
   const mostRecent = locationData[0];
-  const value = logged ? Math.log10(mostRecent[similarityMetric]) : mostRecent[similarityMetric];
+  const locationValue = mostRecent[similarityMetric];
+  const value = logged ? Math.log10(locationValue) : locationValue;
 
   const thresholdString = logged ? `${Math.pow(10, (1-threshold) * value)} TO ${Math.pow(10, (1+threshold) * value)}` : `${(1-threshold) * value} TO ${(1+threshold) * value}`;
 
@@ -105,13 +105,22 @@ export function getSimilarData(apiUrl, locationData, similarityMetric, logged = 
 
   return getAll(
     apiUrl,
-    `mostRecent:true AND ${similarityMetric}:[${thresholdString}]&fields=location_id`
+    `mostRecent:true AND ${similarityMetric}:[${thresholdString}]&fields=location_id,${similarityMetric}`
   ).pipe(
+    map(similar => {
+      similar.forEach(d => {
+        d["valueDiff"] = Math.abs(d[similarityMetric] - locationValue);
+      })
+
+      similar.sort((a, b) => a.valueDiff - b.valueDiff);
+
+      const filtered = similar.slice(0, num2Return + 1);
+      return (filtered)
+    }),
     catchError(e => {
-      console.log("%c Error in getting map data!", "color: red");
+      console.log("%c Error in getting similar locations!", "color: red");
       console.log(e);
       return from([]);
-    }),
-    finalize(() => (store.state.admin.loading = false))
+    })
   );
 }
