2bf4ac053d93b150539df954b2c67d992695951d
Laura Hughes <flaneuse@users.noreply.github.com>
2020-08-31 09:42:19 -0700

    remove last vestiges of dynamic plotting

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -19,7 +19,7 @@
         efficiently collecting, sharing, and integrating data is critical
         to scientific research.
       </p>
-      <p class="text-dark h-100 d-flex flex-column justify-content-between focustext font-weight-bold">
+      <p class="text-dark h-100 focustext font-weight-bold">
         <b class="text-highlight">outbreak.info</b> is a resource to
         aggregate all this information into a single location.
       </p>
@@ -42,7 +42,7 @@
           platform to find publications, clinical trials, datasets, protocols, and more.
         </p> -->
 
-        <video class="w-100 mb-3" controls style="background-color: #fff;" >
+        <video class="w-100 mb-3" controls style="background-color: #fff;">
           <source src="@/assets/home/resources_demo.mp4" type="video/mp4">
           <!-- <source src="@/assets/home/resources_demo.ogv" type="video/ogg"> -->
           Your browser does not support the video tag.
@@ -97,7 +97,7 @@
           Explore and download epidemiological data on confirmed cases, deaths, hospitalizations, and testing. Compare between locations to visualize how the pandemic has varied over time and geography.
         </p> -->
 
-        <video class="w-100 mb-3" controls style="background-color: #fff;" >
+        <video class="w-100 mb-3" controls style="background-color: #fff;">
           <!-- <video class="w-100 mb-3" autoplay loop muted> -->
           <source src="@/assets/home/epi_demo.mp4" type="video/mp4">
           <!-- <source src="@/assets/home/epi_demo.ogv" type="video/ogg"> -->
@@ -211,49 +211,7 @@
         <section class="w-100 p-3 card" id="regional-epi-curves">
           <router-link :to="{name: 'Regions'}" class="text-dark h-100 d-flex flex-column justify-content-between">
             <h5 class="text-uppercase">Explore regions</h5>
-            <template v-if="nestedData && nestedData.length > 0">
-              <div class="region-tooltip-plots" v-for="(region, idx) in regionDict" :key="idx">
-                <div class="tooltip-countries" :id="idx" :style="{
-                  visibility: region.display ? 'visible' : 'hidden',
-                  left: region.x + 'px',
-                  top: region.y + 'px'
-                }">
-                  <div>
-                    {{ region.region }}
-                  </div>
-                  <div>{{ region.currentCases }} total {{ selectedVariable }}</div>
-                  <div class="click-affordance py-1" :style="{ background: lightColor(region.region) }">
-                    click for details
-                  </div>
-                </div>
-                <CountryBarGraph :region="region.region" :variable="selectedVariable" :id="idx" :style="{
-                  visibility: region.displayMore ? 'visible' : 'hidden'
-                }" @regionSelected="handleTooltip" class="tooltip-countries-detailed" />
-              </div>
-            </template>
-
-            <template v-if="nestedData && nestedData.length > 0">
-              <h6>
-                Cumulative Number of COVID-19
-                <select v-model="selectedVariable" class="select-dropdown" @change="changeVariable">
-                  <option v-for="option in variableOptions" :value="option.value" :key="option.value">
-                    {{ option.label }}
-                  </option>
-                </select>
-                by Region
-              </h6>
-            </template>
-
-            <div id="regional-stacked-area-plots d-flex" ref="regional_stacked_area_plots">
-              <div class="row px-2" v-if="nestedData && nestedData.length > 0">
-                <div class="col-sm-12 col-md-12">
-                  <EpiStacked :width="stackedWidth" :height="stackedHeight" :data="nestedData" :includeChinaAnnot="true" id="all-data" :title="`${selectedVariableLabel} Worldwide`" @regionSelected="handleTooltip" />
-
-                </div>
-              </div>
-            </div>
           </router-link>
-          <!-- <DataSource v-if="nestedData && nestedData.length > 0" class="mx-4" :data="nestedData" dataType="regions" :ids="['NYT', 'JHU']" figureRef="epi-summary-svg" /> -->
         </section>
       </div>
 
@@ -263,7 +221,7 @@
           <router-link :to="{name: 'Doubling Rates'}" class="text-dark h-100 d-flex flex-column">
             <h5 class="text-uppercase">View doubling rates</h5>
             <div class="h-100 d-flex flex-column justify-content-center">
-            <img src="@/assets/home/doubling_example.svg" alt="Outbreak.info doubling rates" class="w-100" />
+              <img src="@/assets/home/doubling_example.svg" alt="Outbreak.info doubling rates" class="w-100" />
             </div>
 
           </router-link>
@@ -292,10 +250,6 @@
     </div>
   </section>
 
-
-
-
-
   <section class="container">
     <p class="focustext">
       Notice a bug, know of a COVID-19 data source, or want to suggest a
@@ -310,78 +264,28 @@
 </template>
 <script>
 // @ is an alias to /src
-import EpiStacked from "@/components/EpiStacked.vue";
-import CountryBarGraph from "@/components/CountryBarGraph.vue";
 import SearchBar from "@/components/SearchBar.vue";
 import Logos from "@/components/Logos.vue";
-import {
-  getStackedRegions
-} from "@/api/region-summary.js";
-import {
-  getEpiTable
-} from "@/api/epi-traces.js";
-import {
-  getMapData
-} from "@/api/epi-geo.js";
-import {
-  getCurrentDate
-} from "@/api/biothings.js";
-import {
-  getLocation
-} from "@/js/get-location.js";
 
 import {
   mapState
 } from "vuex";
-
-import {
-  cloneDeep
-} from "lodash";
 
 import store from "@/store";
 
 export default {
   name: "Home",
   components: {
-    EpiStacked,
-    CountryBarGraph,
     SearchBar,
     Logos
   },
   data() {
     return {
-      stackedWidth: 500,
-      stackedHeight: 250,
-      data: null,
-      dataSubscription: null,
-      tableSubscription: null,
-      nestedData: null,
-      selectedVariable: "confirmed",
-      variableOptions: [{
-          label: "Cases",
-          value: "confirmed"
-        },
-        {
-          label: "Recoveries",
-          value: "recovered"
-        },
-        {
-          label: "Deaths",
-          value: "dead"
-        }
-      ],
       searchQuery: ""
     };
   },
-  watch: {},
   computed: {
-    ...mapState("admin", ["loading"]),
-    ...mapState("geo", ["regionDict"]),
-    selectedVariableLabel() {
-      return this.variableOptions.filter(
-        d => d.value == this.selectedVariable
-      )[0]["label"];
-    }
+    ...mapState("admin", ["loading"])
   },
   methods: {
     submitSearch() {
@@ -391,119 +295,12 @@ export default {
           q: this.searchQuery
         }
       });
-    },
-    changeVariable() {
-      this.nestedData = this.data[this.selectedVariable];
-    },
-    handleTooltip(selected) {
-      store.commit("geo/setRegionTooltip", selected);
-    },
-    regionColorScale: function(location) {
-      const scale = store.getters["colors/getRegionColorFromLocation"];
-      return scale(location);
-    },
-    lightColor: function(region) {
-      const scale = store.getters["colors/getRegionColor"];
-      return scale(region, 0.85);
-    },
-    setDims() {
-      const selector = this.$refs.regional_stacked_area_plots;
-
-      if (selector) {
-        const dims = selector.getBoundingClientRect();
-        // const dims = {window.innerWidth, height: window.innerHeight}
-        const whRatio = 5 / 3;
-        const widthThresh = 700;
-        const selectorsProportion = 0.8;
-
-        this.stackedWidth = dims.width;
-        const idealHeight = this.stackedWidth / whRatio;
-        if (idealHeight < window.innerHeight * selectorsProportion) {
-          this.stackedHeight = idealHeight * selectorsProportion;
-        } else {
-          this.stackedHeight = window.innerHeight * selectorsProportion;
-          this.stackedWidth = this.stackedHeight * whRatio;
-        }
-      }
     }
-  },
-  subscriptions() {
-    return {
-      mapData$: getMapData(this.$apiurl),
-      currentDate$: getCurrentDate(this.$apiurl)
-    };
-  },
-  mounted() {
-    getLocation(this.$apiurl);
-
-    this.dataSubscription = getStackedRegions(this.$apiurl).subscribe(d => {
-      this.data = d;
-      this.nestedData = d[this.selectedVariable];
-    });
-
-    this.tableSubscription = getEpiTable(
-      this.$apiurl,
-      null,
-      [0, 1, 2],
-      "-confirmed",
-      10,
-      0
-    ).subscribe(_ => null);
-
-    // Event listener for mobile responsiveness
-    // $nextTick waits till DOM rendered
-    this.$nextTick(function() {
-      window.addEventListener("resize", this.setDims);
-      // set initial dimensions for the stacked area plots.
-      this.setDims();
-    });
-  },
-  destroyed() {
-    this.dataSubscription.unsubscribe();
-    this.tableSubscription.unsubscribe();
-    window.removeEventListener("resize", this.setDims);
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.tooltip-countries {
-    background: white;
-    position: fixed;
-    box-shadow: 2px 2px 6px rgba(0, 0, 0, 0.5);
-    padding: 10px;
-    z-index: 1000;
-    pointer-events: none;
-}
-
-.tooltip-countries-detailed {
-    background: white;
-    position: fixed;
-    box-shadow: 2px 2px 6px rgba(0, 0, 0, 0.5);
-    padding: 10px;
-    z-index: 1001;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-}
-
-.click-affordance {
-    width: 100%;
-    text-align: center;
-    font-size: 0.85em;
-}
-
-.text-spacing-1 {
-    letter-spacing: 1px;
-    word-spacing: 3px;
-}
-
-.header-banner {
-    // background-image: url("~@/assets/texture.png");
-    // background-repeat: no-repeat;
-    // background-size: cover;
-}
-
 .resources-intro {
     background: $primary-color;
 }
