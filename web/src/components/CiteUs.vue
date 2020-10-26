ad2ec941a2e6550abe7101e9e2f8259ec4f80150
Laura Hughes <flaneuse@users.noreply.github.com>
2020-10-26 09:44:00 -0500

    minor update to citation text

--- web/src/components/CiteUs.vue
+++ web/src/components/CiteUs.vue
@@ -7,8 +7,10 @@
     <p class="m-0">
       {{outbreak.authors}} <i>outbreak.info</i>. Available online: <a href="https://outbreak.info/" target="_blank">https://outbreak.info/</a> (2020)
     </p>
-    <small class="d-block">
-      <span class="font-weight-500">data license</span>: <a href="https://creativecommons.org/publicdomain/zero/1.0/" target="_blank">CC0</a>
+    <small class="d-block mt-2">
+      <span class="font-weight-500">data license</span>:
+      All data and content that our team creates is covered by the
+      <a href="https://creativecommons.org/publicdomain/zero/1.0/" target="_blank" rel="noreferrer">Creative Commons CC0 Waiver</a>, which states that you can copy, modify, distribute and perform the work, even for commercial purposes, all without asking permission. However, consistent with scientific norms, we ask that you cite <router-link to="/">outbreak.info</router-link> in any tools or publications that use <router-link to="/">outbreak.info</router-link>.
     </small>
 
   </div>
