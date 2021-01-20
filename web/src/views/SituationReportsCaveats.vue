2b67bbf9dbfc8dc55c3369058d53995b3fb980c9
flaneuse <flaneuse@users.noreply.github.com>
2021-01-20 11:27:06 -0600

    update css

--- web/src/views/SituationReportsCaveats.vue
+++ web/src/views/SituationReportsCaveats.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="container">
-    <h1>
+  <div class="container text-left my-5">
+    <h1 class="m-0">
       SARS-CoV-2 Mutation Situation Report Caveats
     </h1>
   </div>
