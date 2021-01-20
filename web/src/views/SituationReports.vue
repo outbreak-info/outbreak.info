3470518a1e849d6ee03efbea2511a38f4d5e7a64
flaneuse <flaneuse@users.noreply.github.com>
2021-01-20 15:17:45 -0600

    remove archived reports

--- web/src/views/SituationReports.vue
+++ web/src/views/SituationReports.vue
@@ -3,43 +3,44 @@
   <h1 class="m-0">SARS-CoV-2 Mutation Situation Reports</h1>
   <div class="mb-1">
     <div class="d-flex flex-column justify-content-center align-items-center">
-      <div>The <a href="https://andersen-lab.com/" rel="noreferrer" target="_blank">Andersen Lab</a> at Scripps Research is tracking the prevalence of several mutations or groups of mutations (lineages) within the SARS-CoV-2 genome. Every two days, we will produce a report describing the current situation, focusing on the United States.</div>
-        <!-- <router-link :to="{name:'SituationReport'}" class="btn btn-main-outline mt-3">How to interpret these reports</router-link> -->
+      <div>The <a href="https://andersen-lab.com/" rel="noreferrer" target="_blank">Andersen Lab</a> at Scripps Research is tracking the prevalence of several mutations or groups of mutations (lineages) within the SARS-CoV-2 genome. Every two days,
+        we will produce a report describing the current situation, focusing on the United States.</div>
+      <!-- <router-link :to="{name:'SituationReport'}" class="btn btn-main-outline mt-3">How to interpret these reports</router-link> -->
     </div>
 
     <div class="logo-group d-flex align-items-center justify-content-center border-top border-bottom w-100 py-1 my-3">
-    <div class="logo mr-4">
-      <a href="https://andersen-lab.com/" rel="noreferrer" target="_blank">
-        <img src="@/assets/anderson-logo-light.png" class="w-100" alt="Anderson Lab" />
-      </a>
-    </div>
+      <div class="logo mr-4">
+        <a href="https://andersen-lab.com/" rel="noreferrer" target="_blank">
+          <img src="@/assets/anderson-logo-light.png" class="w-100" alt="Anderson Lab" />
+        </a>
+      </div>
 
-    <div class="col-sm-2 d-flex justify-content-center align-items-center picBox">
-      <a href="https://cvisb.org/" rel="noreferrer" target="_blank">
-        <img src="@/assets/cvisb-logo-light.png" class="w-100" alt="CVISB" />
-      </a>
-    </div>
+      <div class="col-sm-2 d-flex justify-content-center align-items-center picBox">
+        <a href="https://cvisb.org/" rel="noreferrer" target="_blank">
+          <img src="@/assets/cvisb-logo-light.png" class="w-100" alt="CVISB" />
+        </a>
+      </div>
 
-    <div class="logo mr-4">
-      <a href="https://globalhealth.scripps.edu/" rel="noreferrer" target="_blank">
-        <img src="@/assets/global-logo-light.png" class="w-100" alt="SDCGH" />
-      </a>
-    </div>
+      <div class="logo mr-4">
+        <a href="https://globalhealth.scripps.edu/" rel="noreferrer" target="_blank">
+          <img src="@/assets/global-logo-light.png" class="w-100" alt="SDCGH" />
+        </a>
+      </div>
 
-    <div class="logo mr-4">
-      <a href="https://searchcovid.info/" rel="noreferrer" target="_blank">
-        <img src="@/assets/search-logo.png" class="w-100" alt="SEARCH Alliance" />
-      </a>
-    </div>
+      <div class="logo mr-4">
+        <a href="https://searchcovid.info/" rel="noreferrer" target="_blank">
+          <img src="@/assets/search-logo.png" class="w-100" alt="SEARCH Alliance" />
+        </a>
+      </div>
 
-    <div class="logo mr-4">
-      <a href="https://outbreak.info/" rel="noreferrer" target="_blank">
-        <img src="@/assets/logo-full-01.svg" class="w-100" alt="outbreak.info" />
-      </a>
-    </div>
+      <div class="logo mr-4">
+        <a href="https://outbreak.info/" rel="noreferrer" target="_blank">
+          <img src="@/assets/logo-full-01.svg" class="w-100" alt="outbreak.info" />
+        </a>
+      </div>
     </div>
     <div class="d-flex flex-column justify-content-center align-items-center">
-        <router-link :to="{name:'SituationReportCaveats'}" class="btn btn-main-outline">How to interpret these reports</router-link>
+      <router-link :to="{name:'SituationReportCaveats'}" class="btn btn-main-outline">How to interpret these reports</router-link>
     </div>
 
 
@@ -48,12 +49,16 @@
     <div class="mutation-group mb-5" v-for="(group, i) in reports" :key="i">
       <h2>{{ group.key }}</h2>
       <div class="report-group mb-3" v-for="(mutation, mIdx) in group.values" :key="mIdx">
-        <h5 class="m-0">{{ mutation.key }}</h5>
-        <ul class="d-flex flex-column m-0">
-          <li v-for="(report, rIdx) in mutation.values" :key="rIdx">
-            <router-link :to="{name:'SituationReport', params:{date: report.date, mutation: report.name, url: report.url }}">{{ rIdx === 0 ? `current (${report.date})` : report.date }}</router-link>
-          </li>
-        </ul>
+        <div v-for="(report, rIdx) in mutation.values" :key="rIdx">
+          <template v-if="rIdx === 0">
+          <router-link :to="{name:'SituationReport', params:{date: report.date, mutation: report.name, url: report.url }}">
+            <h5 class="m-0">{{ mutation.key }}</h5>
+          </router-link>
+          <router-link :to='{name:"Resources", query:{q: `"${report.name}"`}}' class="ml-3" v-if="report.name === 'B.1.1.7'">
+            <small>View {{report.name}} resources</small>
+          </router-link>
+        </template>
+        </div>
         <!-- <span @click="showAll(mutation.key)">view older</span> -->
       </div>
     </div>
@@ -67,9 +72,13 @@ import Vue from "vue";
 
 import axios from "axios";
 
-import { nest } from "d3";
+import {
+  nest
+} from "d3";
 
-import { orderBy } from "lodash";
+import {
+  orderBy
+} from "lodash";
 
 export default {
   name: "SituationReports",
@@ -85,9 +94,9 @@ export default {
       response.data = orderBy(response.data, ["lineage", "name", "date"], ["asc", "asc", "desc"])
 
       this.reports = nest()
-      .key(d => d.type)
-      .key(d => d.name)
-      .entries(response.data);
+        .key(d => d.type)
+        .key(d => d.name)
+        .entries(response.data);
     })
   }
 }
