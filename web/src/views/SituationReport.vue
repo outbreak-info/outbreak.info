a1882071b8eedbf97e61a91dbb9a93311a0b143c
flaneuse <flaneuse@users.noreply.github.com>
2021-01-22 18:04:04 -0600

    add social media/share icons

--- web/src/views/SituationReport.vue
+++ web/src/views/SituationReport.vue
@@ -1,5 +1,11 @@
 <template>
-<div class="my-5 mx-4 full-page text-left">
+<div class="my-4 mx-4 full-page text-left">
+  <div class="d-flex w-100 justify-content-end text-muted mb-2">
+    <font-awesome-icon class="ml-3" :icon="['fas', 'share']" />
+    <font-awesome-icon class="ml-3" :icon="['fab', 'twitter']" />
+    <font-awesome-icon class="ml-3" :icon="['fas', 'envelope']" />
+    <font-awesome-icon class="ml-3" :icon="['fas', 'link']" />
+  </div>
   <div class="d-flex justify-content-between align-items-center">
     <div class="d-flex flex-column align-items-start">
       <h1 class="m-0">{{ mutationName }} {{ reportType | capitalize }} Report</h1>
@@ -25,7 +31,8 @@
   <div class="row">
     <section id="intro" class="col-sm-6 col-md-8 pr-4">
       <div class="font-size-2">
-        Concerns surrounding a new strain of SARS-CoV-2, the virus behind the COVID-19 pandemic, have been developing. <b class="text-highlight">B.1.1.7</b> lineage, also known as <b>Variant of Concern 202012/01 (VOC-202012/01)</b> or <b>20B/501Y.V1</b>, was first identified in the UK in early December 2020 and has since been detected in the US and other counties. This is of growing concern because it has shown to be significantly more transmissible than other variants.
+        Concerns surrounding a new strain of SARS-CoV-2, the virus behind the COVID-19 pandemic, have been developing. <b class="text-highlight">B.1.1.7</b> lineage, also known as <b>Variant of Concern 202012/01 (VOC-202012/01)</b> or
+        <b>20B/501Y.V1</b>, was first identified in the UK in early December 2020 and has since been detected in the US and other counties. This is of growing concern because it has shown to be significantly more transmissible than other variants.
       </div>
       <router-link :to='{name:"Resources", query:{q: `"${mutationName}"`}}'>
         <small>View publications, datasets, and more related to {{mutationName}}</small>
@@ -78,64 +85,64 @@
       <div class="summary-counts mb-3">
         As of {{ dateUpdated }}, <b>{{ totalSeqs.toLocaleString() }}</b> sequences containing the variations in {{ mutationName }} have been detected:
 
-      <table class="border-bottom line-height-1 mt-2 w-100">
-        <thead>
-          <tr class="border-bottom">
-            <th>
-              location
-            </th>
-            <th class="text-center">
-              sequence count
-            </th>
-            <th class="text-center">
-              apparent prevalence<sup>*</sup>
-            </th>
-          </tr>
-        </thead>
-        <tbody class="checkbook">
-          <tr>
-            <td>
-              Worldwide
-            </td>
-            <td class="text-center">
-              {{ totalSeqs.toLocaleString() }}
-            </td>
-            <td class="text-center">
-              5.6%
-            </td>
-          </tr>
-          <tr>
-            <td>
-              United States
-            </td>
-            <td class="text-center">
-              180
-            </td>
-            <td  class="text-center">
-              0.2%
-            </td>
-          </tr>
-          <tr>
-            <td>
-              San Diego County
-            </td>
-            <td class="text-center">
-              1
-            </td>
-            <td class="text-center">
-              &lt; 0.1%
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <small class="bright-hyperlink"><a href="#longitudinal">view change over time</a></small>
-    </div>
+        <table class="border-bottom line-height-1 mt-2 w-100">
+          <thead>
+            <tr class="border-bottom">
+              <th>
+                location
+              </th>
+              <th class="text-center">
+                sequence count
+              </th>
+              <th class="text-center">
+                apparent prevalence<sup>*</sup>
+              </th>
+            </tr>
+          </thead>
+          <tbody class="checkbook">
+            <tr>
+              <td>
+                Worldwide
+              </td>
+              <td class="text-center">
+                {{ totalSeqs.toLocaleString() }}
+              </td>
+              <td class="text-center">
+                5.6%
+              </td>
+            </tr>
+            <tr>
+              <td>
+                United States
+              </td>
+              <td class="text-center">
+                180
+              </td>
+              <td class="text-center">
+                0.2%
+              </td>
+            </tr>
+            <tr>
+              <td>
+                San Diego County
+              </td>
+              <td class="text-center">
+                1
+              </td>
+              <td class="text-center">
+                &lt; 0.1%
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <small class="bright-hyperlink"><a href="#longitudinal">view change over time</a></small>
+      </div>
 
-    <div id="geo-summary">
-      The strain has been detected in at least <b>{{ countries.length }} {{countries.length === 1 ? "country" : "countries"}}</b> and <b>{{ states.length }} U.S. {{states.length === 1 ? "state" : "states"}}</b>.
-      <CountryMap :countries="countries" :width="400" :showNames="false" />
-      <small class="bright-hyperlink"><a href="#geographic">view geographic prevalence</a></small>
-    </div>
+      <div id="geo-summary">
+        The strain has been detected in at least <b>{{ countries.length }} {{countries.length === 1 ? "country" : "countries"}}</b> and <b>{{ states.length }} U.S. {{states.length === 1 ? "state" : "states"}}</b>.
+        <CountryMap :countries="countries" :width="400" :showNames="false" />
+        <small class="bright-hyperlink"><a href="#geographic">view geographic prevalence</a></small>
+      </div>
     </section>
   </div>
 
@@ -147,10 +154,12 @@
   <section class="my-3">
     <h4>Methodology</h4>
     <p class="m-0">
-      All SARS-CoV sequences were downloaded from the <a href="https://www.gisaid.org/" rel="noreferrer" target="_blank">GISAID Initiative</a> on {{ dateUpdated }} and aligned to reference sequence <a :href="refSeq.url" target="_blank" rel="noreferrer">{{refSeq.name}}</a>.
+      All SARS-CoV sequences were downloaded from the <a href="https://www.gisaid.org/" rel="noreferrer" target="_blank">GISAID Initiative</a> on {{ dateUpdated }} and aligned to reference sequence <a :href="refSeq.url" target="_blank"
+        rel="noreferrer">{{refSeq.name}}</a>.
     </p>
     <small class=""><a @click="downloadGISAID" href="">Download associated GISAID IDs</a></small>
-    <Warning class="mt-2" text="B.1.1.7 genomes in the US were identified by S-gene target failures (SGTF) in community-based diagnostic PCR testing. Since this is not an unbiased approach, it does not indicate the true prevalence of the B117 lineage in the US.  <a class='text-light text-underline' href='https://outbreak.info/situation-reports/caveats'>How to interpret this report</a>"/>
+    <Warning class="mt-2"
+      text="B.1.1.7 genomes in the US were identified by S-gene target failures (SGTF) in community-based diagnostic PCR testing. Since this is not an unbiased approach, it does not indicate the true prevalence of the B117 lineage in the US.  <a class='text-light text-underline' href='https://outbreak.info/situation-reports/caveats'>How to interpret this report</a>" />
 
   </section>
 
@@ -181,8 +190,16 @@ import {
 import {
   faClock
 } from "@fortawesome/free-regular-svg-icons";
+import {
+  faLink,
+  faShare,
+  faEnvelope
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faTwitter
+} from "@fortawesome/free-brands-svg-icons";
 
-library.add(faClock);
+library.add(faLink, faShare, faEnvelope, faTwitter, faClock);
 
 
 export default {
@@ -197,7 +214,10 @@ export default {
   data() {
     return {
       mutationName: "B.1.1.7",
-      refSeq: {url: "https://www.ncbi.nlm.nih.gov/nuccore/NC_045512.2/", name: "NC_045512"},
+      refSeq: {
+        url: "https://www.ncbi.nlm.nih.gov/nuccore/NC_045512.2/",
+        name: "NC_045512"
+      },
       reportType: "lineage",
       lastUpdated: "2 hours",
       dateUpdated: "22 January 2021",
@@ -224,19 +244,18 @@ export default {
 }
 
 .checkbook td {
-  padding: 0.5rem;
+    padding: 0.5rem;
 }
 
 .checkbook tr:nth-child(2n+1) {
-  background-color: lighten($primary-color, 7%)
+    background-color: lighten($primary-color, 7%);
 }
 
 .font-size-2 {
-font-size: 1.25rem;
+    font-size: 1.25rem;
 }
 
 .vis {
-  background: aliceblue;
+    background: aliceblue;
 }
-
 </style>
