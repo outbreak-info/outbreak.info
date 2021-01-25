8117dee5c43ea1c8e0f204e7f973b8eaa4ef588f
flaneuse <flaneuse@users.noreply.github.com>
2021-01-25 09:16:26 -0600

    fix gisaid logo ref

--- web/src/views/SituationReport.vue
+++ web/src/views/SituationReport.vue
@@ -18,7 +18,7 @@
       <div class="d-flex align-items-center">
         Generated using data from
         <a href="https://www.gisaid.org/" rel="noreferrer" target="_blank">
-          <img src="@/assets/gisaid.png" class="gisaid ml-1" alt="GISAID Initiative" />
+          <img src="@/assets/resources/gisaid.png" class="gisaid ml-1" alt="GISAID Initiative" />
         </a>
       </div>
       <small class="mr-1"><a @click="downloadGISAID" href="">Download associated GISAID IDs</a></small>
