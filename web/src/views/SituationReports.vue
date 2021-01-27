5f6b25d877f54a12acc46c4a5019c23e4ad08020
flaneuse <flaneuse@users.noreply.github.com>
2021-01-27 09:26:36 -0600

    New list of curated seqs, card format

--- web/src/views/SituationReports.vue
+++ web/src/views/SituationReports.vue
@@ -27,38 +27,44 @@
 
   </div>
   <section id="report-list" class="text-left">
-    <div class="mutation-group mb-5" v-for="(group, i) in reports" :key="i">
-      <h2 class="mb-0">{{ group.key }} Reports</h2>
-      <small class="text-highlight">{{group.key == "Lineage" ? "sequences classified as a particular Pangolin lineage" : "sequences with a particular mutation(s)" }}</small>
-      <div class="report-group mb-1 card col-sm-4" v-for="(mutation, mIdx) in group.values" :key="mIdx">
-        <div v-for="(report, rIdx) in mutation.values" :key="rIdx">
-          <div class="d-flex align-items-end justify-content-between w-100">
-            <template v-if="rIdx === 0">
-              <div class="d-flex flex-column mr-3 mutation-name">
-                <div class="d-flex align-items-center">
-                  <router-link :to="{name:'SituationReport', params:{date: report.date, mutation: report.name, url: report.url }}">
-                    <h5 class="m-0 pb-1 mr-3"><b>{{ mutation.key }}</b></h5>
-                  </router-link>
-                  <div class="VOC" v-if="mIdx < 3">Variant of Concern</div>
-                  <div class="VOI" v-if="mIdx >= 3">Variant of Interest</div>
-                </div>
+    <!-- lineage or mutation -->
+    <div class="mutation-group mb-5" v-for="(group, i) in reports" :key="i" id="report-group">
+      <h2 class="mb-0">{{ group.key | capitalize }} Reports</h2>
+      <small class="text-highlight">{{group.key.toLowerCase() == "lineage" ? "sequences classified as a particular Pangolin lineage" : "sequences with a particular mutation(s)" }}</small>
 
-                <small>a.k.a.: </small>
-                <small><em>first identified in</em></small>
-              </div>
+      <!-- report cards (heh) -->
+      <div class="row mt-3">
+        <div class="col-sm-12 col-md-6 col-lg-6 mb-3 d-flex report-group" v-for="(report, rIdx) in group.values" :key="rIdx" id="mutation-report">
+          <div class="w-100 p-3 card">
+            <!-- NAME -->
+            <div class="d-flex justify-content-between" id="mutation-name">
+              <router-link :to="{name:'SituationReport', params:{ mutation: report.identifier }}">
+                <h5 class="m-0 pb-1 mr-3"><b>{{ report.mutation_name }}</b></h5>
+              </router-link>
+              <div class="VOC" v-if="report.variantType == 'Variant of Concern'">Variant of Concern</div>
+              <div class="VOI" v-if="report.variantType == 'Variant of Interest'">Variant of Interest</div>
+            </div>
+            <!-- DESCRIPTION -->
+            <small v-if="report.location_first_identified"><em>first identified in {{ report.location_first_identified }}</em></small>
+            <small v-if="report.mutation_synonyms"><span>a.k.a. </span>
+              <span v-for="(synonym, sIdx) in report.mutation_synonyms" :key="sIdx">
+                <b>{{ synonym }}</b>
+                <span v-if="sIdx < report.mutation_synonyms.length - 1">, </span></span>
+            </small>
 
-              <div class="mutation-map flex-grow-1">
-                <SARSMutationMap :mutationKey="mutation.key" />
-              </div>
-            </template>
+            <!-- LINK TO RESOURCES -->
+            <router-link :to='{name:"Resources", query:{q: `"${report.mutation_name}"`}}' v-if="report.mutation_name === 'B.1.1.7'">
+              <small>View {{report.mutation_name}} resources</small>
+            </router-link>
+
+            <!-- MUTATION MAP / DEFINITION -->
+            <div class="mutation-map flex-grow-1 px-2">
+              <SARSMutationMap :mutationKey="report.mutation_name" />
+            </div>
           </div>
-          <router-link :to='{name:"Resources", query:{q: `"${report.name}"`}}' class="ml-3" v-if="rIdx === 0 && report.name === 'B.1.1.7'">
-            <small>View {{report.name}} resources</small>
-          </router-link>
         </div>
-
-        <!-- <span @click="showAll(mutation.key)">view older</span> -->
       </div>
+
     </div>
   </section>
 
@@ -99,18 +105,17 @@ export default {
   },
   data() {
     return {
-      reports: null,
-      maxReports: 2
+      // reminder: must be the raw verison of the file
+      curatedFile: "https://raw.githubusercontent.com/andersen-lab/hCoV19-sitrep/master/curated_mutations.json",
+      reports: null
     }
   },
   mounted() {
-    axios.get("https://raw.githubusercontent.com/andersen-lab/hCoV19-sitrep/master/metadata.json").then(response => {
-      // response.data.sort((a,b) => (b.type > a.type ? -1 : 1) || (b.name > a.name ? -1 : 1) || b.date > a.date ? 1 : -1);
-      response.data = orderBy(response.data, ["lineage", "name", "date"], ["asc", "asc", "desc"])
+    axios.get(this.curatedFile).then(response => {
+      response.data = orderBy(response.data, ["reportType", "variantType", "mutation_name"]);
 
       this.reports = nest()
-        .key(d => d.type)
-        .key(d => d.name)
+        .key(d => d.reportType)
         .entries(response.data);
     })
   }
@@ -140,7 +145,8 @@ $mutation-width: 275px;
     min-width: 0;
 }
 
-.VOC, .VOI {
+.VOC,
+.VOI {
     padding: 0.35rem;
     border-radius: 0.25rem;
     border-style: solid;
@@ -151,16 +157,15 @@ $mutation-width: 275px;
     font-weight: 700;
 }
 
-
 .VOC {
-  background: lighten($publication-color, 35%);
-  color: $publication-color;
-  border-color: $publication-color;
+    background: lighten($publication-color, 35%);
+    color: $publication-color;
+    border-color: $publication-color;
 }
 
 .VOI {
-  background: lighten($website-color, 40%);
-  color: $website-color;
-  border-color: $website-color;
+    background: lighten($website-color, 40%);
+    color: $website-color;
+    border-color: $website-color;
 }
 </style>
