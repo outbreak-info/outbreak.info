be44022b863cb152fe5660b7b0b6dd1390183bd4
flaneuse <flaneuse@users.noreply.github.com>
2021-01-28 12:08:54 -0600

    testing checkbooks, lollipops

--- web/src/components/ReportPrevalenceByLocation.vue
+++ web/src/components/ReportPrevalenceByLocation.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="">
-  <svg :width="width" :height="height" class="epi-curve" ref="svg_dot" :name="title">
+  <svg :width="width" :height="height" class="dotplot-prevalence" ref="svg_dot" :name="title">
     <g :transform="`translate(${margin.left}, ${height - margin.bottom })`" class="prevalence-axis axis--x" ref="xAxis"></g>
     <g :transform="`translate(${margin.left}, ${margin.top})`" class="prevalence-axis axis--y" ref="yAxis"></g>
     <g ref="dotplot" id="dotplot" :transform="`translate(${margin.left}, ${margin.top})`"></g>
@@ -41,7 +41,7 @@ export default Vue.extend({
     },
     height: {
       type: Number,
-      default: 800
+      default: 1000
     },
     sortVar: {
       type: String,
@@ -70,7 +70,7 @@ export default Vue.extend({
         left: 200
       },
       bandHeight: 15,
-      circleR: 9,
+      circleR: 8,
       ciStrokeWidth: 7,
       // refs
       dotplot: null,
@@ -141,57 +141,87 @@ export default Vue.extend({
     drawPlot() {
       const t1 = transition().duration(1500);
 
+      // const checkbookSelector = select(this.$refs.svg_dot)
+      //   .selectAll(".checkbook")
+      //   .data(this.data.filter((d, i) => (i+1) % 2));
+      //
+      // checkbookSelector.join(enter => {
+      //   enter.append("rect")
+      //     .attr("class", "checkbook")
+      //     .style("fill", "#EEEEEE")
+      //     .style("fill-opacity", 0.8)
+      //     .attr("x", 0)
+      //     .attr("width", this.width)
+      //     .attr("y", d => this.y(d[this.yVariable]))
+      //     .attr("height", this.y.bandwidth())
+      //
+      // })
       const dotSelector = this.dotplot.selectAll(".dot-group")
         .data(this.data, d => d[this.yVariable]);
 
       dotSelector.join(enter => {
-        const grp = enter.append("g")
-          .attr("class", d => `dot-group ${d[this.yIdentifier]}`);
+          const grp = enter.append("g")
+            .attr("class", d => `dot-group ${d[this.yIdentifier]}`);
 
-        grp.append("line")
-          .attr("class", "dot-ci confidence-interval")
-          .attr("x1", d => this.xDot(d.lower))
-          .attr("x2", d => this.xDot(d.upper))
-          .attr("y1", d => this.y(d[this.yVariable]) + this.y.bandwidth() / 2)
-          .attr("y2", d => this.y(d[this.yVariable]) + this.y.bandwidth() / 2)
-          .style("stroke", "#DDDDDD")
-          .style("stroke-width", this.ciStrokeWidth)
-          .style("opacity", 0)
-          .transition(t1)
-          .delay(400)
-          .style("opacity", 1);
+          grp.append("line")
+            .attr("class", "dot-ci confidence-interval")
+            .attr("x1", d => 0)
+            .attr("x2", d => this.xDot(d.est))
+            .attr("y1", d => this.y(d[this.yVariable]) + this.y.bandwidth() / 2)
+            .attr("y2", d => this.y(d[this.yVariable]) + this.y.bandwidth() / 2)
+            .style("stroke", "#555555")
+            .style("stroke-width", 1)
+            .style("display", "none")
+            .transition(t1)
 
-        grp.append("circle")
-          .attr("class", "dot-circle point-estimate")
-          .attr("cy", d => this.y(d[this.yVariable]) + this.y.bandwidth() / 2)
-          .attr("r", this.circleR)
-          .style("stroke", "#2c3e50")
-          .style("stroke-width", 0.25)
-          .style("fill", d => this.colorScale(d.est))
-          .transition(t1)
-          .attr("cx", d => this.xDot(d.est));
-      },
-      update => {
-        update.selectAll(".dot-ci")
-          .attr("x1", d => this.xDot(d.lower))
-          .attr("x2", d => this.xDot(d.upper))
-          .attr("y1", d => this.y(d[this.yVariable]) + this.y.bandwidth() / 2)
-          .attr("y2", d => this.y(d[this.yVariable]) + this.y.bandwidth() / 2)
-          .style("opacity", 0)
-          .transition(t1)
-          .delay(400)
-          .style("opacity", 1);
+          grp.append("line")
+            .attr("class", "dot-ci confidence-interval")
+            .attr("x1", d => this.xDot(d.lower))
+            .attr("x2", d => this.xDot(d.upper))
+            .attr("y1", d => this.y(d[this.yVariable]) + this.y.bandwidth() / 2)
+            .attr("y2", d => this.y(d[this.yVariable]) + this.y.bandwidth() / 2)
+            .style("stroke", "#CCCCCC")
+            .style("stroke-width", this.ciStrokeWidth)
+            .style("opacity", 0)
+            .transition(t1)
+            .delay(400)
+            .style("opacity", 0.5);
 
-        update.selectAll(".point-estimate")
-          .transition(t1)
-          .style("fill", d => this.colorScale(d.est))
-          .attr("cx", d => this.xDot(d.est));
-      },
-      exit => exit.call(exit => exit.transition().duration(10).style("opacity", 1e-5).remove()))
+          grp.append("circle")
+            .attr("class", "dot-circle point-estimate")
+            .attr("cy", d => this.y(d[this.yVariable]) + this.y.bandwidth() / 2)
+            .attr("r", this.circleR)
+            .style("stroke", "#2c3e50")
+            .style("stroke-width", 0.25)
+            .style("fill", d => this.colorScale(d.est))
+            .transition(t1)
+            .attr("cx", d => this.xDot(d.est));
+        },
+        update => {
+          update.selectAll(".dot-ci")
+            .attr("x1", d => this.xDot(d.lower))
+            .attr("x2", d => this.xDot(d.upper))
+            .attr("y1", d => this.y(d[this.yVariable]) + this.y.bandwidth() / 2)
+            .attr("y2", d => this.y(d[this.yVariable]) + this.y.bandwidth() / 2)
+            .style("opacity", 0)
+            .transition(t1)
+            .delay(400)
+            .style("opacity", 1);
+
+          update.selectAll(".point-estimate")
+            .transition(t1)
+            .style("fill", d => this.colorScale(d.est))
+            .attr("cx", d => this.xDot(d.est));
+        },
+        exit => exit.call(exit => exit.transition().duration(10).style("opacity", 1e-5).remove()))
     }
   }
 })
 </script>
 
 <style lang="scss">
+g.axis--y g.tick line,
+g.axis--y path {
+    display: none;
+}
 </style>
