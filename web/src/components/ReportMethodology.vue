4ec8feadcb6958e4156f2f4abc772d75b2d13f70
flaneuse <flaneuse@users.noreply.github.com>
2021-01-29 09:32:29 -0600

    add date updated to report methodology

--- web/src/components/ReportMethodology.vue
+++ web/src/components/ReportMethodology.vue
@@ -5,8 +5,10 @@
       rel="noreferrer">{{refSeq.name}}</a>.
   </p>
   <p class="m-0">
-    Mutation prevalence was calculated as a ratio of the count of sequences containing a given set of mutations on a day at a particular location (or in all locations) (<b>x</b>) divided by the total sequences on that data in that location (<b>n</b>). 95% Confidence Intervals for these prevalences were calculated according to Jeffrey's Interval:
-     <span class="code" v-html="'2.5 quantile of	&beta;(x + 0.5, n - x + 0.5)'"></span> to <span class="code" v-html="'97.5 quantile of &beta;(x + 0.5, n - x + 0.5)'"></span>. For longitudinal analyses, sequences with collection dates specifying only the year or month and year were excluded.
+    Mutation prevalence was calculated as a ratio of the count of sequences containing a given set of mutations on a day at a particular location (or in all locations) (<b>x</b>) divided by the total sequences on that data in that location
+    (<b>n</b>). 95% Confidence Intervals for these prevalences were calculated according to Jeffrey's Interval:
+    <span class="code" v-html="'2.5 quantile of	&beta;(x + 0.5, n - x + 0.5)'"></span> to <span class="code" v-html="'97.5 quantile of &beta;(x + 0.5, n - x + 0.5)'"></span>. For longitudinal analyses, sequences with collection dates specifying only
+    the year or month and year were excluded.
   </p>
 </div>
 </template>
@@ -21,15 +23,19 @@ import {
 
 export default {
   name: "SituationReports",
-
   computed: {
     ...mapState("genomics", ["refSeq"])
+  },
+  data() {
+    return {
+      dateUpdated: "26 January 2021"
+    }
   }
 }
 </script>
 <style lang = "scss">
 .code {
-  color: $secondary-color;
-  font-family: "monospace";
+    color: $secondary-color;
+    font-family: "monospace";
 }
 </style>
