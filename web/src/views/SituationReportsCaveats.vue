0129861e27adb903ddfab9d787ddad4bcd9c96a5
Alaa Abdel Latif <al.a.latif.94@gmail.com>
2021-02-09 01:32:44 -0800

    minor additions on inherent limitations of epi-based sampling

--- web/src/views/SituationReportsCaveats.vue
+++ web/src/views/SituationReportsCaveats.vue
@@ -7,13 +7,13 @@
     <h3>Caveats &amp; Limitations</h3>
     <p class="mt-3 w-75">
       To describe the prevalence of sets of mutations in our <router-link :to="{name: 'SituationReports'}">Mutation Situation Reports</router-link>, we rely on shared virus sequences from the <a href="https://www.gisaid.org/" target="_blank">GISAID
-        Initiative</a>. These sequences are a sample of the total number of cases &mdash; often a biased sample &mdash; and do not represent the true prevalence of the mutations in the population.
+        Initiative</a>. These sequences are a sample of the total number of cases &mdash; often a biased sample &mdash; and may not represent the true prevalence of the mutations in the population. In general, it is important to note that case numbers for any given lineage/mutation can be significantly affected by overall case numbers and rates of genomic sequencing at any given location. 
     </p>
 
     <p class="w-75">
-      Additionally, sequencing techniques can bias this sampling. For instance, the majority of <router-link :to="{ name:'SituationReport', params: { mutation: 'B.1.1.7'} }">B.1.1.7 genomes</router-link> identified in the United States to date were
+      Additionally, sequencing techniques can heavily bias this sampling. For instance, the majority of <router-link :to="{ name:'SituationReport', params: { mutation: 'B.1.1.7'} }">B.1.1.7 genomes</router-link> identified in the United States to date were
       identified by S-gene target failures (SGTF)
-      in community-based diagnostic PCR testing.
+      in community-based diagnostic PCR testing - a test that specifically identifies genomes such as B.1.1.7. 
       SGTF indicates a deletion mutation that is one of several mutations able to distinguish the B.1.1.7 from other SARS-CoV-2 strains.
       Estimates of true prevalence in the U.S. are discussed in the <a href="https://blog.helix.com/b117-variant-updated-data/ target='_blank'">Helix Research blog</a>.
     </p>
