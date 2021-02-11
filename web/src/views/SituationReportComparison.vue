b672bd5abeb1c366a512e2f66a3e1ebaa519bd6f
flaneuse <flaneuse@users.noreply.github.com>
2021-02-11 08:52:10 -0600

    minor adjustments to format; add table

--- web/src/views/SituationReportComparison.vue
+++ web/src/views/SituationReportComparison.vue
@@ -2,20 +2,30 @@
 <div class="my-4 mx-4 half-page text-left">
   <h1>Comparing two sets of mutations</h1>
   <div id="select-lineages">
-    <div class="d-flex align-items-center my-2 w-100">
+    <div class="d-flex align-items-end mt-3 mb-5 w-100" id="mutation-1">
       <div class="w-400px mr-4">
         <TypeaheadSelect :queryFunction="queryPangolin" :selected="mutant1" @selected="updatePangolin1" :apiUrl="this.$genomicsurl" :removeOnSelect="false" placeholder="Select lineage 1" />
       </div>
-
-      <SARSMutationMap :mutationArr="mutations1" :mutationKey="mutant1" class="flex-1" />
+      <div>
+        <h5>
+          {{ mutant1 }}
+        </h5>
+        <SARSMutationMap :mutationArr="mutations1" :mutationKey="mutant1" class="w-600px" />
+      </div>
+        <CharacteristicMutations :mutationName="mutant1" :mutations="mutations1" :definitionLabel="mutant1" class="" />
     </div>
-    <div class="d-flex align-items-center my-2 w-100">
+
+    <div class="d-flex align-items-end mt-3 mb-5 w-100" id="mutation-2">
       <div class="w-400px mr-4">
-        <TypeaheadSelect class="mr-4" :queryFunction="queryPangolin" :selected="mutant2" @selected="updatePangolin2" :apiUrl="this.$genomicsurl" :removeOnSelect="false" placeholder="Select lineage 2" />
+        <TypeaheadSelect :queryFunction="queryPangolin" :selected="mutant2" @selected="updatePangolin2" :apiUrl="this.$genomicsurl" :removeOnSelect="false" placeholder="Select lineage 2" />
       </div>
-      <div class="mutation-map flex-grow-1 px-2">
-        <SARSMutationMap :mutationArr="mutations2" :mutationKey="mutant2" />
+      <div>
+        <h5>
+          {{ mutant2 }}
+        </h5>
+        <SARSMutationMap :mutationArr="mutations2" :mutationKey="mutant2" class="w-600px" />
       </div>
+        <CharacteristicMutations :mutationName="mutant2" :mutations="mutations2" :definitionLabel="mutant2" class="" />
     </div>
   </div>
 </div>
@@ -30,13 +40,15 @@ import {
 
 import SARSMutationMap from "@/components/SARSMutationMap.vue";
 import TypeaheadSelect from "@/components/TypeaheadSelect.vue";
+import CharacteristicMutations from "@/components/CharacteristicMutations.vue";
 
 
 export default {
   name: "SituationReportsDemo",
   components: {
     TypeaheadSelect,
-    SARSMutationMap
+    SARSMutationMap,
+    CharacteristicMutations
   },
   data() {
     return {
@@ -63,6 +75,7 @@ export default {
       if (this.mutant1) {
         this.mutation1Subscription = getCharacteristicMutations(this.$genomicsurl, this.mutant1).subscribe(muts => {
           this.mutations1 = muts;
+          console.log(this.mutations1)
         })
       }
     },
@@ -74,6 +87,8 @@ export default {
       }
     },
     updatePangolin1(selected) {
+      this.mutant1 = selected.name;
+      this.getMutants1();
       this.$router.push({
         name: "SituationReportComparison",
         query: {
@@ -83,6 +98,8 @@ export default {
       })
     },
     updatePangolin2(selected) {
+      this.mutant2 = selected.name;
+      this.getMutants2();
       this.$router.push({
         name: "SituationReportComparison",
         query: {
@@ -98,5 +115,8 @@ export default {
 <style lang="scss" scoped>
 .w-400px {
     width: 200px;
+}
+.w-600px {
+    width: 600px;
 }
 </style>
