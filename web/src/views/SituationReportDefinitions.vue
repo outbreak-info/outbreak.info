2b62573d8f63b41b602d1a422f1c83108858c42e
EmilyHaag <75391907+EmilyHaag@users.noreply.github.com>
2021-02-24 12:18:37 -0800

    Update SituationReportDefinitions.vue

--- web/src/views/SituationReportDefinitions.vue
+++ web/src/views/SituationReportDefinitions.vue
@@ -1,4 +1,68 @@
-<template>
-<div>
-</div>
-</template>
+<html>
+  <head>  
+    <title>Situation Report Definitions</title>  
+</head>
+  <body>
+<h1><strong><span style="font-size: 14px; font-family: Arial, Helvetica, sans-serif;">Glossary of Mutations Terms</span></strong></h1>
+<ul style="list-style-type: disc; margin-left: 0in;">
+<li><strong><span style="font-size: 11px; font-family: Arial, Helvetica, sans-serif;">Mutant</span><span style="font-size: 11px;"><span style="font-family: Arial, Helvetica, sans-serif;">:</span></span></strong><span style="font-size: 11px;"><span style="font-family: Arial, Helvetica, sans-serif;">&nbsp;error during viral replication</span></span>
+<ol style="list-style-type: circle;">
+<li><span style="font-size: 11px;"><span style="font-family: Arial, Helvetica, sans-serif;">Could be good (e.g. the mutation actually kills the virus), bad (e.g. the mutation increases the transmissibility of the virus), or no effect (e.g. the mutation does not create characteristics that differ from the original virus)</span></span></li>
+<li><span style="font-size: 11px;"><span style="font-family: Arial, Helvetica, sans-serif;"><strong>Deletion:</strong> one or more nucleotides are left out of the genetic sequence during replication</span></span></li>
+<li><span style="font-size: 11px;"><span style="font-family: Arial, Helvetica, sans-serif;"><strong>Insertion:</strong> one or more extra nucleotides are added into the genetic sequence</span></span></li>
+<li><span style="font-size: 11px;"><span style="font-family: Arial, Helvetica, sans-serif;"><strong>Substitution:</strong> one nucleotide is switched with another</span></span></li>
+</ol>
+</li>
+</ul>
+<ul>
+<li><span style="font-size: 11px;"><span style="font-family: Arial, Helvetica, sans-serif;"><strong>Strain:</strong> Viral genome that has distinct properties and causes a specific immune response</span></span></li>
+</ul>
+<ul style="list-style-type: disc;">
+<li><span style="font-size: 11px;"><span style="font-family: Arial, Helvetica, sans-serif;"><strong>Variant:</strong> A genome that contains a particular set of mutations that causes it to act differently than that of the original strain</span></span>
+<ol style="list-style-type: circle;">
+<li><span style="font-size: 11px;"><span style="font-family: Arial, Helvetica, sans-serif;"><strong>Variant of Concern (VOC):</strong> variants which have evidence in the literature of increased transmissibility, worse outcomes, and/or decreased vaccine efficacy</span></span></li>
+<li><span style="font-size: 11px;"><span style="font-family: Arial, Helvetica, sans-serif;"><strong>Variant of Interest (VOI):</strong> variants of increasing prevalence with mutations in biologically important regions but lacking evidence for increased transmissibility, virulence, and/or immune evasion, also called <strong>Variant Under Investigation (VUI)</strong></span></span></li>
+</ol>
+</li>
+</ul>
+<ul style="list-style-type: disc; margin-left: 0in;">
+<li><span style="font-size: 11px;"><span style="font-family: Arial, Helvetica, sans-serif;"><strong>Lineage:</strong> All the descendants of a variant</span></span>
+<ol style="list-style-type: circle;">
+<li><span style="font-size: 11px;"><span style="font-family: Arial, Helvetica, sans-serif;">Within a lineage, there may be additional mutations which revert some changes or accumulate new ones</span></span></li>
+<li><strong><span style="font-size: 11px; font-family: Arial, Helvetica, sans-serif;">PANGO Lineage:</span></strong><span style="font-family: Arial, Helvetica, sans-serif; font-size: 11px;">&nbsp;<a href="https://cov-lineages.org/pangolin.html">Phylogenetic Assignment of Named Global Outbreak Lineages</a></span></li>
+</ol>
+</li>
+</ul>
+<ul>
+<li><span style="font-size: 11px;"><span style="font-family: Arial, Helvetica, sans-serif;"><strong>Nucleotide:</strong> building blocks of DNA and RNA that provide the code necessary to store and transmit genetic information</span></span></li>
+<li><span style="font-size: 11px;"><span style="font-family: Arial, Helvetica, sans-serif;"><strong>Codon:</strong> sequence of three nucleotides</span></span></li>
+<li><span style="font-size: 11px;"><span style="font-family: Arial, Helvetica, sans-serif;"><strong>Phylogenetic tree:</strong> a &ldquo;family tree&rdquo; showing how mutations are related to each other</span></span></li>
+</ul>
+<p><img src="https://i.postimg.cc/RFPBTHkR/tree.png" alt="" width="400" height="369" />&nbsp;</p>
+<ul>
+<li style="list-style-type: none;">
+<ul>
+<li><span style="font-size: 11px;"><span style="font-family: Arial, Helvetica, sans-serif;">What it does</span></span>
+<ul style="list-style-type: square;">
+<li><span style="font-size: 11px;"><span style="font-family: Arial, Helvetica, sans-serif;">Used to determine lineages</span></span></li>
+<li><span style="font-size: 11px;"><span style="font-family: Arial, Helvetica, sans-serif;">Shows how strains of viruses are related to each other</span></span></li>
+<li><span style="font-size: 11px;"><span style="font-family: Arial, Helvetica, sans-serif;">Used to trace how/when/where mutations originated</span></span></li>
+</ul>
+</li>
+<li><span style="font-size: 11px;"><span style="font-family: Arial, Helvetica, sans-serif;">When the same mutations show up in two variants, this can mean two things:&nbsp;</span></span>
+<ul style="list-style-type: square;">
+<li><span style="font-size: 11px;"><span style="font-family: Arial, Helvetica, sans-serif;">The virus was derived from the same lineage</span></span></li>
+<li><span style="font-size: 11px;"><span style="font-family: Arial, Helvetica, sans-serif;">Convergent evolution</span></span></li>
+</ul>
+</li>
+</ul>
+</li>
+<li><span style="font-size: 11px;"><span style="font-family: Arial, Helvetica, sans-serif;"><strong>Convergent evolution:</strong> Selective pressure</span></span>
+<ol style="list-style-type: circle;">
+<li><span style="font-size: 11px;"><span style="font-family: Arial, Helvetica, sans-serif;">Randomly, two variants of a virus pop up with the same mutation without the cases being connected. This suggests selective pressure &ndash; the virus has mutated to improve biological fitness.</span></span>&nbsp;</li>
+</ol>
+</li>
+<li><strong><span style="font-size: 11px; font-family: Arial, Helvetica, sans-serif;">Apparent prevalence:</span></strong><span style="font-family: Arial, Helvetica, sans-serif; font-size: 11px;">&nbsp;The ratio of the sequences containing the variant to all sequences (variant and non-variant) collected since the identification of the variant in a location</span></li>
+</ul>
+</body>
+</html>
