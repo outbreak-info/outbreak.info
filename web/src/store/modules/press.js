5aca45fec8b727b32de4a48fb931c3821853104b
flaneuse <flaneuse@users.noreply.github.com>
2021-02-25 08:44:57 -0600

    Create press.js

---
+++ web/src/store/modules/press.js
@@ -0,0 +1,23 @@
+const state = {
+press: [
+  "https://www.nytimes.com/2021/02/24/health/coronavirus-variant-nyc.html",
+  "https://www.pressdemocrat.com/article/news/california-variant-of-the-coronavirus-identified-in-marin-lake-counties/"
+]
+}
+
+// getters
+const getters = {};
+
+// actions
+const actions = {};
+
+// mutations
+const mutations = {};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
+};
