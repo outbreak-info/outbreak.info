97972ca44ab75a843f302f573f060898d09caaaa
flaneuse <flaneuse@users.noreply.github.com>
2021-02-26 12:10:55 -0600

    Add Science News article

--- web/src/store/modules/press.js
+++ web/src/store/modules/press.js
@@ -1,6 +1,7 @@
 const state = {
 press: [
   "https://www.nytimes.com/2021/02/24/health/coronavirus-variant-nyc.html",
+  "https://www.sciencemag.org/news/2021/02/coronavirus-strain-first-identified-california-may-be-more-infectious-and-cause-more",
   "https://www.pressdemocrat.com/article/news/california-variant-of-the-coronavirus-identified-in-marin-lake-counties/",
   "https://nymag.com/intelligencer/2021/02/new-york-city-coronavirus-variant-b-1-526-what-we-know.html"
 ]
