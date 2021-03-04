8b090e7672bad1741a8d40fb516addde74cfabb3
flaneuse <flaneuse@users.noreply.github.com>
2021-03-04 13:06:37 -0600

    x-zoom, tooltips

--- web/src/components/LineagesByLocation.vue
+++ web/src/components/LineagesByLocation.vue
@@ -1,11 +1,25 @@
 <template>
 <div>
+  <div class="d-flex justify-content-end px-3" :style="{width: width + 'px'}">
+    <button class="btn btn-accent-flat text-highlight d-flex align-items-center m-0 p-2" @click="enableZoom">
+      <font-awesome-icon class="text-right" :icon="['fas', 'search-plus']" />
+    </button>
+    <button class="btn btn-accent-flat text-highlight d-flex align-items-center m-0 p-2" @click="resetZoom">
+      <font-awesome-icon class="text-right" :icon="['fas', 'compress-arrows-alt']" />
+    </button>
+  </div>
+
   <svg :width="width" :height="height" class="lineages-by-location" ref="lineages_by_location" :name="title">
     <g :transform="`translate(${margin.left},${margin.top})`" ref="chart">
     </g>
     <g class="stream-axis axis--x" ref="xAxis" :transform="`translate(${margin.left},${height - margin.bottom})`"></g>
     <g class="stream-axis axis--y" ref="yAxis" :transform="`translate(${margin.left},${margin.top})`"></g>
+    <g ref="brush" class="brush" id="brush-zoom" :transform="`translate(${margin.left},${margin.top})`" v-if="data" :class="{hidden: !zoomAllowed}"></g>
   </svg>
+  <div ref="tooltip_streamgraph" class="tooltip-basic box-shadow" id="tooltip-mutation">
+    <h5 class="my-1">
+    </h5>
+  </div>
 </div>
 </template>
 
@@ -14,6 +28,20 @@
 import Vue from "vue";
 
 import uniq from "lodash/uniq";
+
+// --- font awesome --
+import {
+  FontAwesomeIcon
+} from "@fortawesome/vue-fontawesome";
+import {
+  library
+} from "@fortawesome/fontawesome-svg-core";
+import {
+  faSearchPlus,
+  faCompressArrowsAlt
+} from "@fortawesome/free-solid-svg-icons/";
+
+library.add(faSearchPlus, faCompressArrowsAlt);
 
 import {
   select,
@@ -28,15 +56,18 @@ import {
   // stackOrderAscending,
   // stackOrderDescending,
   event,
+  brushX,
   extent,
-  format,
-  max
+  max,
+  format
 } from "d3";
 
 
 export default Vue.extend({
   name: "LineagesByLocation",
-  components: {},
+  components: {
+    FontAwesomeIcon
+  },
   props: {
     data: Array,
     colorScale: Function
@@ -58,7 +89,7 @@ export default Vue.extend({
     return ({
       // dimensions
       margin: {
-        top: 10,
+        top: 8,
         bottom: 30,
         left: 55,
         right: 10
@@ -76,16 +107,32 @@ export default Vue.extend({
       numYTicks: 5,
       // methods
       area: null,
+      brush: null,
       // data
       series: null,
       lineages: null,
       // refs
-      chart: null
+      chart: null,
+      brushRef: null,
+      // controls
+      zoomAllowed: false
     })
   },
   mounted() {
     this.$nextTick(function() {
       window.addEventListener("resize", this.debounceSetDims);
+
+      // Update brush so it spans the whole of the area
+      this.brush = brushX()
+        .extent([
+          [0, 0],
+          [this.width - this.margin.left - this.margin.right, this.height - this.margin.top - this.margin.bottom]
+        ])
+        .on("end", () => this.debounceZoom(event));
+
+      this.brushRef
+        .call(this.brush)
+        .on("dblclick", this.resetZoom);
     })
 
     // set initial dimensions for the plots.
@@ -95,6 +142,7 @@ export default Vue.extend({
   },
   created: function() {
     this.debounceSetDims = this.debounce(this.setDims, 150);
+    this.debounceZoom = this.debounce(this.zoom, 150);
   },
   methods: {
     setDims() {},
@@ -102,6 +150,7 @@ export default Vue.extend({
       this.svg = select(this.$refs.svg);
       this.legend = select(this.$refs.legend);
       this.chart = select(this.$refs.chart);
+      this.brushRef = select(this.$refs.brush);
 
       this.area = area()
         .x(d => this.x(d.data.date_time))
@@ -111,7 +160,8 @@ export default Vue.extend({
     updateScales() {
       this.x = this.x
         .range([0, this.width - this.margin.left - this.margin.right])
-        .domain(extent(this.data.map(d => d.date_time)));
+        .domain(extent(this.data.map(d => d.date_time)))
+        .clamp(true);
 
       this.y = this.y
         .range([0, this.height - this.margin.top - this.margin.bottom])
@@ -143,6 +193,68 @@ export default Vue.extend({
         this.drawPlot();
       }
     },
+    tooltipOn(key) {
+      const ttipShift = 20;
+      // turn everything off
+      this.chart
+        .selectAll(".stacked-area-chart")
+        .style("stroke", "#BBB")
+        .style("fill-opacity", 0.2);
+      // turn on the selected region
+      this.chart
+        .select(`#area_${key.replace(/\./g, "-")}`)
+        .style("fill-opacity", 1);
+      const ttip = select(this.$refs.tooltip_streamgraph);
+
+      ttip.select("h5")
+        .text(key)
+      // fix location
+      ttip
+        .style("left", `${event.clientX + ttipShift}px`)
+        .style("top", `${event.clientY + ttipShift}px`)
+        .style("display", "block");
+    },
+    tooltipOff() {
+      this.chart
+        .selectAll(".stacked-area-chart")
+        .style("stroke", "#555")
+        .style("fill-opacity", 1);
+
+      select(this.$refs.tooltip_streamgraph)
+        .style("display", "none");
+    },
+    zoom(evt) {
+      // reset domain to new coords
+      const selection = this.event.selection;
+
+      if (selection) {
+        const newMin = this.x.invert(selection[0]);
+        const newMax = this.x.invert(selection[1]);
+        this.x = this.x
+          .range([0, this.width - this.margin.left - this.margin.right])
+          .domain([newMin, newMax])
+          .clamp(true);
+        // reset the axis
+        this.xAxis = axisBottom(this.x)
+          .ticks(this.numXTicks);
+
+        select(this.$refs.xAxis).call(this.xAxis);
+
+        // move the brush
+        this.brushRef.call(this.brush.move, null);
+        this.zoomAllowed = false;
+        this.drawPlot();
+      }
+
+    },
+    resetZoom() {
+      this.x = this.x.domain(extent(this.data.map(d => d.date_time)));
+      this.brushRef.call(this.brush.move, null);
+      this.updatePlot();
+    },
+    enableZoom() {
+      this.zoomAllowed = true;
+    },
     drawPlot() {
       const areaSelector = this.chart
         .selectAll(".stacked-area-chart")
@@ -150,34 +262,28 @@ export default Vue.extend({
 
       areaSelector
         .join(enter => {
-            enter.append("path")
+            const selector = enter.append("path")
+              .attr("fill", ({
+                key
+              }) => this.colorScale(key))
+              .attr("class", "stacked-area-chart")
+              .attr("id", ({
+                key
+              }) => `area_${key.replace(/\./g, "-")}`)
+              .attr("d", this.area)
+              .style("stroke", "#555")
+              .style("stroke-width", 0.5)
+          },
+          update => {
+            update
               .attr("fill", ({
                 key
               }) => this.colorScale(key))
               .attr("id", ({
                 key
               }) => `area_${key.replace(/\./g, "-")}`)
-              .attr("d", this.area)
-              .style("stroke", "white")
-              .style("stroke-width", 0.5)
-              .append("title")
-              .text(({
-                key
-              }) => key)
+              .attr("d", this.area);
           },
-          update =>
-          update
-          .attr("fill", ({
-            key
-          }) => this.colorScale(key))
-          .attr("id", ({
-            key
-          }) => `area_${key.replace(/\./g, "-")}`)
-          .attr("d", this.area)
-          .append("title")
-          .text(({
-            key
-          }) => key),
           exit =>
           exit.call(exit =>
             exit
@@ -186,6 +292,13 @@ export default Vue.extend({
             .remove()
           )
         )
+
+      this.chart
+        .selectAll(".stacked-area-chart")
+        .on("mouseenter", ({
+          key
+        }) => this.tooltipOn(key))
+        .on("mouseleave", this.tooltipOff)
     },
     debounce(fn, delay) {
       var timer = null;
