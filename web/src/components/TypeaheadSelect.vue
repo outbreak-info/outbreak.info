e82aaadfe822389e2d36f04c507c92b811f7e3de
flaneuse <flaneuse@users.noreply.github.com>
2021-03-09 08:48:46 -0600

    Revert "Updated typeahead to show label"
    This reverts commit afecd3cd89666ef27467a001ff0b2e88b3db76cd.

--- web/src/components/TypeaheadSelect.vue
+++ web/src/components/TypeaheadSelect.vue
@@ -4,8 +4,7 @@
     <input :class="{ 'form-control': isStandalone }" :disabled="disabled" type="text" v-model="selected" :placeholder="placeholder" @keydown.enter='enter' @keydown.down='down' @keydown.up='up' @input='debounceSearch' />
     <div class="dropdown-menu" :class="{'show':isOpen}" style="width:100%">
       <a v-for="(suggestion, idx) in matches" :key="idx" class="dropdown-item" :class="{'active': isActive(idx)}" @click="suggestionClick(idx)">
-        {{ suggestion.label }}
-      </a>
+        {{ suggestion.name }} ({{ suggestion.total_count.toLocaleString() }} {{ totalLabel }})</a>
     </div>
   </div>
 
@@ -111,7 +110,7 @@ export default {
       if (this.removeOnSelect || !this.selected) {
         this.selected = null; // reset
       } else {
-        this.selected = this.selected.label;
+        this.selected = this.selected.name;
       }
     }
   }
