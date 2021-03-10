93b5b860d1c54d6c463be7a509d922b41a322aa6
flaneuse <flaneuse@users.noreply.github.com>
2021-03-09 21:12:42 -0600

    add comparison header

--- web/src/views/SituationReportComparison.vue
+++ web/src/views/SituationReportComparison.vue
@@ -1,85 +1,129 @@
 <template>
-<div class="my-4 mx-4 half-page text-left">
-  <div class="d-flex flex-wrap align-items-center">
-    <h1 class="mr-3">Lineage comparison</h1>
-    <div class="d-flex flex-wrap">
-      <button role="button" class="btn chip btn-outline-secondary bg-white d-flex align-items-center py-1 px-2 line-height-1" v-for="(lineage, lIdx) in selectedPango" :key="lIdx" @click="deletePango(lIdx)">
-        <span>{{lineage}}</span>
-        <font-awesome-icon class="ml-1" :icon="['far', 'times-circle']" :style="{'font-size': '0.85em', 'opacity': '0.6'}" />
-      </button>
+<div>
+  <div class="my-2 mx-4 half-page text-left">
+    <!-- SOCIAL MEDIA SHARE, BACK BTN -->
+    <div class="d-flex align-items-center">
+      <ShareReport title="title" url="url" />
     </div>
 
-  </div>
-  <div id="select-lineages" class="mb-3 p-2 bg-white border-top border-bottom">
-    <h5>Selected lineages</h5>
-    <div class="d-flex flex-wrap">
-      <button role="button" class="btn chip btn-outline-secondary bg-white d-flex align-items-center py-1 px-2 line-height-1" v-for="(lineage, lIdx) in selectedPango" :key="lIdx" @click="deletePango(lIdx)">
-        <span>{{lineage}}</span>
-        <font-awesome-icon class="ml-1" :icon="['far', 'times-circle']" :style="{'font-size': '0.85em', 'opacity': '0.6'}" />
-      </button>
-      <button role="button" class="btn chip btn-main d-flex align-items-center py-1 px-2 mx-3 line-height-1" @click="clearPango()">
-        clear lineages
-      </button>
-      <div style="width: 150px">
-        <TypeaheadSelect :queryFunction="queryPangolin" @selected="addPango" :apiUrl="this.$genomicsurl" :removeOnSelect="true" placeholder="Add lineage" />
-      </div>
-      <button @click="addMutations()">Add E484K</button>
-    </div>
-  </div>
+    <!-- REPORT HEADER -->
+    <div class="d-flex flex-column text-light comparison-banner py-3" :class="[smallScreen ? 'mx-n2 px-2' : 'mx-n5 px-5']">
+      <h3 class="m-0 mt-n1 text-grey">SARS-CoV-2 (hCoV-19) Mutation Reports</h3>
+      <div class="d-flex justify-content-between align-items-center">
+        <div class="d-flex flex-column align-items-start">
 
-  <!-- CHECKBOX TO SELECT GENES -->
-  <div id="select-genes" class="d-flex flex-wrap align-items-center justify-content-between">
-    <div class="d-flex flex-wrap">
-      <small class="text-muted mr-2 align-self-start">include genes:</small>
-      <label class="b-contain pr-3" v-for="(gene, idx) in geneOpts" :key="idx">
-        <span>{{gene}}</span>
-        <input type="checkbox" :id="gene" :value="gene" v-model.lazy="selectedGenes" @change="updateGenes()" />
-        <div class="b-input"></div>
-      </label>
-    </div>
-
-  </div>
+          <div class="d-flex align-items-end">
+            <div class="d-flex align-items-center">
+              <h1 class="m-0 font-weight-bold comparison-header">Lineage Comparison</h1>
+              <button class="btn py-1 px-2 mx-4 btn-grey flex-shrink-0" data-toggle="modal" data-target="#change-locations-modal">
+                <font-awesome-icon class="mr-2 font-size-small" :icon="['fas', 'plus']" />add lineages
+              </button>
+            </div>
+          </div>
+          <div class="d-flex align-items-center">
+            <small class="text-muted badge bg-grey__lightest mt-1" v-if="lastUpdated">
+              <font-awesome-icon class="mr-1" :icon="['far', 'clock']" /> Updated {{ lastUpdated }} ago
+            </small>
+            <div class="text-light font-size-2 ml-5" v-if="totalSequences">
+              {{totalSequences}} sequences
+            </div>
+          </div>
 
 
-
-  <!-- LOOP OVER MUTATION HEATMAPS -->
-  <div id="mutation-heatmaps">
-    <div class="d-flex flex-wrap justify-content-between">
-      <div>
-        <h3 class="m-0">Mutation prevalence across lineages</h3>
-        <p class="text-muted m-0">Mutations with > {{ prevalenceThresholdFormatted }} prevalence in at least one lineage</p>
-      </div>
-
-      <!-- LEGEND -->
-      <div id="legend" class="d-flex bg-dark px-2 py-1 mx-4">
-        <GradientLegend maxValue="100%" :colorScale="colorScale" :dark="true" label="Mutation prevalence in lineage" class="mr-3" />
-        <div class="d-flex align-items-center">
-          <svg width="24" height="24">
-            <defs>
-              <pattern id="diagonalHatch" width="5" height="5" patternTransform="rotate(45 0 0)" patternUnits="userSpaceOnUse">
-                <line x1="0" y1="0" x2="0" y2="10" :style="`stroke:#AAA; stroke-width:0.75`" />
-              </pattern>
-            </defs>
-            <rect x="2" y="2" width="20" height="20" fill="url(#diagonalHatch)" rx="4" stroke="#888" stroke-width="0.5"></rect>
-          </svg>
-          <small class="text-light ml-2">not detected</small>
+        </div>
+        <div class="d-flex flex-column align-items-end justify-content-between flex-shrink-0">
+          <div class="d-flex align-items-center mb-1">
+            Enabled by data from
+            <a href="https://www.gisaid.org/" rel="noreferrer" target="_blank">
+              <img src="@/assets/resources/gisaid.png" class="gisaid ml-2" alt="GISAID Initiative" />
+            </a>
+          </div>
+          <div class="d-flex align-items-center bright-hyperlink my-1">
+            <font-awesome-icon class="mr-2" :icon="['fas', 'info-circle']" />
+            <router-link :to="{name:'SituationReportCaveats'}" class="bright-hyperlink">How to interpret these reports</router-link>
+          </div>
         </div>
       </div>
     </div>
 
+    <!-- <div class="d-flex flex-wrap align-items-center">
+      <h1 class="mr-3">Lineage comparison</h1>
+      <div class="d-flex flex-wrap">
+        <button role="button" class="btn chip btn-outline-secondary bg-white d-flex align-items-center py-1 px-2 line-height-1" v-for="(lineage, lIdx) in selectedPango" :key="lIdx" @click="deletePango(lIdx)">
+          <span>{{lineage}}</span>
+          <font-awesome-icon class="ml-1" :icon="['far', 'times-circle']" :style="{'font-size': '0.85em', 'opacity': '0.6'}" />
+        </button>
+      </div>
+    </div> -->
 
-    <div class="d-flex flex-wrap">
-
-      <div v-for="(geneData, gIdx) in mutationHeatmap" :key="gIdx" class="mr-4 mb-2">
-        <template v-if="selectedGenes.includes(geneData.key)">
-          <h4 class="m-0 text-dark">{{ geneData.key }}</h4>
-          <MutationHeatmap :data="geneData.values" :yDomain="selectedPango" />
-        </template>
+    <div id="select-lineages" class="my-3 p-2 bg-white border-top border-bottom">
+      <h5>Selected lineages</h5>
+      <div class="d-flex flex-wrap">
+        <button role="button" class="btn chip btn-outline-secondary bg-white d-flex align-items-center py-1 px-2 line-height-1" v-for="(lineage, lIdx) in selectedPango" :key="lIdx" @click="deletePango(lIdx)">
+          <span>{{lineage}}</span>
+          <font-awesome-icon class="ml-1" :icon="['far', 'times-circle']" :style="{'font-size': '0.85em', 'opacity': '0.6'}" />
+        </button>
+        <button role="button" class="btn chip btn-main d-flex align-items-center py-1 px-2 mx-3 line-height-1" @click="clearPango()">
+          clear lineages
+        </button>
+        <div style="width: 150px">
+          <TypeaheadSelect :queryFunction="queryPangolin" @selected="addPango" :apiUrl="this.$genomicsurl" :removeOnSelect="true" placeholder="Add lineage" />
+        </div>
+        <button @click="addMutations()">Add E484K</button>
       </div>
     </div>
 
-  </div>
+    <!-- LOOP OVER MUTATION HEATMAPS -->
+    <div id="mutation-heatmaps">
+      <div class="d-flex flex-wrap justify-content-between">
+        <div>
+          <h3 class="m-0">Mutation prevalence across lineages</h3>
+          <p class="text-muted m-0">Mutations with > {{ prevalenceThresholdFormatted }} prevalence in at least one lineage</p>
+        </div>
 
+        <!-- LEGEND -->
+        <div id="legend" class="d-flex bg-dark px-2 py-1 mx-4">
+          <GradientLegend maxValue="100%" :colorScale="colorScale" :dark="true" label="Mutation prevalence in lineage" class="mr-3" />
+          <div class="d-flex align-items-center">
+            <svg width="24" height="24">
+              <defs>
+                <pattern id="diagonalHatch" width="5" height="5" patternTransform="rotate(45 0 0)" patternUnits="userSpaceOnUse">
+                  <line x1="0" y1="0" x2="0" y2="10" :style="`stroke:#AAA; stroke-width:0.75`" />
+                </pattern>
+              </defs>
+              <rect x="2" y="2" width="20" height="20" fill="url(#diagonalHatch)" rx="4" stroke="#888" stroke-width="0.5"></rect>
+            </svg>
+            <small class="text-light ml-2">not detected</small>
+          </div>
+        </div>
+      </div>
+
+      <!-- CHECKBOX TO SELECT GENES -->
+      <div id="select-genes" class="d-flex flex-wrap align-items-center justify-content-between mt-3">
+        <div class="d-flex flex-wrap">
+          <small class="text-muted mr-2 align-self-start">include genes:</small>
+          <label class="b-contain pr-3" v-for="(gene, idx) in geneOpts" :key="idx">
+            <span>{{gene}}</span>
+            <input type="checkbox" :id="gene" :value="gene" v-model.lazy="selectedGenes" @change="updateGenes()" />
+            <div class="b-input"></div>
+          </label>
+        </div>
+      </div>
+
+
+      <div class="d-flex flex-wrap">
+
+        <div v-for="(geneData, gIdx) in mutationHeatmap" :key="gIdx" class="mr-4 mb-2">
+          <template v-if="selectedGenes.includes(geneData.key)">
+            <h4 class="m-0 text-dark">{{ geneData.key }}</h4>
+            <MutationHeatmap :data="geneData.values" :yDomain="selectedPango" />
+          </template>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
 </div>
 </template>
 
@@ -112,7 +156,17 @@ import {
   faTimesCircle
 } from "@fortawesome/free-regular-svg-icons";
 
-library.add(faPlus, faTimesCircle);
+import {
+  faInfoCircle
+} from "@fortawesome/free-solid-svg-icons/faInfoCircle";
+import {
+  faSpinner
+} from "@fortawesome/free-solid-svg-icons/faSpinner";
+import {
+  faSync
+} from "@fortawesome/free-solid-svg-icons/faSync";
+
+library.add(faPlus, faTimesCircle, faSpinner, faSync, faInfoCircle);
 
 import {
   interpolateRdPu
@@ -135,27 +189,20 @@ export default {
     },
     gene: {
       type: Array,
-      default: () => ["ORF1a",
-        "ORF1b",
-        "S",
-        "ORF3a",
-        "E",
-        "M",
-        "ORF6",
-        "ORF7a",
-        "ORF7b",
-        "ORF8",
-        "N",
-        "ORF10"
+      default: () => [
+        "s"
       ]
     }
   },
   components: {
-    FontAwesomeIcon,
-    TypeaheadSelect,
-    MutationHeatmap,
-    GradientLegend
-    // CharacteristicMutations
+    TypeaheadSelect: () => import( /* webpackPrefetch: true */ "@/components/TypeaheadSelect.vue"),
+    // ReportMethodology: () => import( /* webpackPrefetch: true */ "@/components/ReportMethodology.vue"),
+    // Warning: () => import( /* webpackPrefetch: true */ "@/components/Warning.vue"),
+    // ReportAcknowledgements: () => import( /* webpackPrefetch: true */ "@/components/ReportAcknowledgements.vue"),
+    ShareReport: () => import( /* webpackPrefetch: true */ "@/components/ShareReport.vue"),
+    MutationHeatmap: () => import( /* webpackPrefetch: true */ "@/components/MutationHeatmap.vue"),
+    GradientLegend: () => import( /* webpackPrefetch: true */ "@/components/GradientLegend.vue"),
+    FontAwesomeIcon
   },
   computed: {
     selectedPango() {
@@ -179,18 +226,30 @@ export default {
       heatmapSubscription: null,
       lineageByMutationsSubscription: null,
       geneOpts: [
-        "ORF1a",
-        "ORF1b",
-        "S",
-        "ORF3a",
-        "E",
-        "M",
-        "ORF6",
-        "ORF7a",
-        "ORF7b",
-        "ORF8",
-        "N",
-        "ORF10"
+        "orf1a",
+        "orf1b",
+        "s",
+        "orf3a",
+        "e",
+        "m",
+        "orf6",
+        "orf7a",
+        "orf7b",
+        "orf8",
+        "n",
+        "orf10"
+        // "ORF1a",
+        // "ORF1b",
+        // "S",
+        // "ORF3a",
+        // "E",
+        // "M",
+        // "ORF6",
+        // "ORF7a",
+        // "ORF7b",
+        // "ORF8",
+        // "N",
+        // "ORF10"
       ]
     }
   },
@@ -220,6 +279,7 @@ export default {
     },
     getData() {
       this.heatmapSubscription = getLineagesComparison(this.$genomicsurl, this.selectedPango, this.prevalenceThreshold).subscribe(results => {
+        console.log(results)
         this.mutationHeatmap = results;
       })
     },
@@ -269,5 +329,9 @@ export default {
 }
 .w-600px {
     width: 600px;
+}
+
+.gisaid {
+    height: 25px;
 }
 </style>
