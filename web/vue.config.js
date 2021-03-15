b9add315c002aa26e1fb64ef1a0a962086a731bf
gkarthik <xraydiffraction@gmail.com>
2021-03-15 13:07:33 -0700

    Added webpackChain to include version number in chunks

--- web/vue.config.js
+++ web/vue.config.js
@@ -6,6 +6,7 @@
 // comment this to disable analyzer
 // plugins.push(new BundleAnalyzerPlugin());
 
+process.env.VUE_APP_VERSION = require('./package.json').version;
 
 APP_DESCRIPTION = "outbreak.info is a standardized, searchable platform to discover and explore COVID-19 and SARS-CoV-2 data from the Center for Viral Systems Biology (cvisb.org) at Scripps Research. It contains three parts: a standardized searchable database of COVID-19 research; customizable real-time surveillance reports on SARS-CoV-2 variants and mutations; and an explorable interface to examine changes in epidemiological data.";
 
@@ -16,6 +17,12 @@ module.exports = {
         prependData: `@import "@/styles/_global.scss";`
       }
     }
+  },
+  chainWebpack: config => {
+    const version = process.env.VUE_APP_VERSION;
+    config.output
+      .filename(`js/[name].[hash:8].${version}.js`)
+      .chunkFilename(`js/[name].[hash:8].${version}.js`)
   },
   configureWebpack: {
     // plugins,
