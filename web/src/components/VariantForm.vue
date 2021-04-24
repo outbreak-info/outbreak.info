f78c236e663241c5d875eb388cbc21382566dd55
flaneuse <flaneuse@users.noreply.github.com>
2021-04-24 11:39:26 -0500

    fix ampersand

--- web/src/components/VariantForm.vue
+++ web/src/components/VariantForm.vue
@@ -45,21 +45,9 @@
 
         <div id="mutation-set" class="ml-5" v-if="selectedType && (selectedType.id == 'mut' || selectedType.id == 'variant' && selectedLineage)">
           <div class="d-flex align-items-center">
-            <!-- <div class="d-flex flex-column align-items-start mr-4 coords p-2" id="coordinate-type"> -->
-            <!--   <h6 class="text-uppercase text-muted">coordinate system</h6> -->
-            <!--   <div class="radio-item"> -->
-            <!--     <input type="radio" id="aa" value="aminoacid" v-model="selectedCoordinate" class="mr-2"> -->
-            <!--     <label for="aminoacid">amino acids</label> -->
-            <!--   </div> -->
-            <!--   <div class="radio-item"> -->
-            <!--     <input type="radio" id="nuc" value="nuc" v-model="selectedCoordinate" class="mr-2"> -->
-            <!--     <label for="nuc">nucleotides</label> -->
-            <!--   </div> -->
-            <!-- </div> -->
-
             <div id="bulk-mutations" class="mr-4 w-350px">
-              <h6 class="text-uppercase text-muted" v-if="!minimalistic">List of mutations</h6>
-              <textarea class="form-control border-theme" v-model="selectedBulkString" placeholder='"gene:mutation": e.g. "S:N501Y, S:DEL69/70"' @input='debounceBulk'></textarea>
+              <small class="text-muted line-height-1">Comma-separated list of mutations, with the gene before the mutation like "S:E484K, S:DEL69/70"</small>
+              <textarea class="form-control border-theme" v-model="selectedBulkString" placeholder='"gene:mutation": e.g. "S:E484K, S:DEL69/70"' @input='debounceBulk'></textarea>
             </div>
             <div class="warning" v-if="badBulkGene && selectedBulkString">
               <p>
@@ -76,68 +64,6 @@
               Specify a deletion like "S:DEL69/70"
             </div>
           </div>
-          <template v-if="!minimalistic">
-            <div class="flex-row d-flex">
-              <div class="mr-4 align-self-center">
-                <b>or</b>
-              </div>
-            </div>
-
-            <div class="flex-row d-flex">
-              <div id="gene-name" class="p-2" v-if="selectedCoordinate === 'aminoacid'">
-                <h6 class="text-uppercase text-muted">gene</h6>
-                <select v-model="selectedGene" class="select-dropdown">
-                  <option v-for="gene in genes" :value="gene" :key="gene.name">
-                    {{gene.name}}
-                  </option>
-                </select>
-              </div>
-
-              <div class="d-flex flex-column align-items-start p-2" id="mutation-type">
-                <h6 class="text-uppercase text-muted">mutation type</h6>
-                <div class="radio-item">
-                  <input type="radio" id="substitution" value="substitution" v-model="selectedMutationType" class="mr-2">
-                  <label for="substitution">substitution</label>
-                </div>
-                <div class="radio-item">
-                  <input type="radio" id="deletion" value="deletion" v-model="selectedMutationType" class="mr-2">
-                  <label for="deletion">deletion</label>
-                </div>
-              </div>
-
-              <div id="location" class="mr-4">
-                <h6 class="text-uppercase text-muted" v-if="selectedMutationType == 'substitution'">location</h6>
-                <h6 class="text-uppercase text-muted" v-else>start location</h6>
-                <input class="form-control border-theme w-110px" v-model="selectedPosition" :placeholder="selectedMax">
-                <small>relative to <a :href="refSeq.url" target="_blank" rel="noreferrer">{{refSeq.name}}</a></small>
-              </div>
-
-              <div id="del-length" class="mr-4" v-if="selectedMutationType == 'deletion'">
-                <h6 class="text-uppercase text-muted">deletion length</h6>
-                <input class="form-control border-theme" :style="{width: '135px'}" v-model="selectedDelLength" :placeholder="`in ${selectedCoordinate}s`">
-              </div>
-
-              <div id="ref_codon" class="mr-4" v-if="selectedRef">
-                <h6 class="text-uppercase text-muted">ref. {{ selectedCoordinate === "aminoacid" ? "amino acid" : "nucleotide"}}</h6>
-                <div>
-                  {{ selectedRef }}
-                </div>
-              </div>
-
-              <div id="new_codon" class="mr-4" v-if="selectedMutationType == 'substitution'">
-                <h6 class="text-uppercase text-muted">new {{ selectedCoordinate === "aminoacid" ? "amino acid" : "nucleotide"}}</h6>
-                <input class="form-control border-theme w-90px" v-model="selectedMutation" :placeholder="mutationPlaceholder">
-              </div>
-
-              <button type="button" class="btn btn-main p-0 d-flex align-self-start align-self-center" role="button" @click="addMutation" :disabled="!addValid">
-                <span class="px-2 py-2">Add <b v-html="selectedLabel"></b>
-                </span>
-                <div class="bg-sec py-2 px-2 border-theme">
-                  <font-awesome-icon :icon="['fas', 'plus']" />
-                </div>
-              </button>
-            </div>
-          </template>
         </div>
 
       </form>
@@ -157,7 +83,7 @@
   </div>
 
 
-  <div class="row flex-column d-flex" v-if="!minimalistic && (selectedType.id == 'mut' || selectedType.id == 'variant')">
+  <div class="row flex-column d-flex" v-if="!minimalistic && selectedType && (selectedType.id == 'mut' || selectedType.id == 'variant')">
     <div class="col-sm-12" v-if="selectedMutations.length">
       <div class="d-flex align-items-start ml-5">
         <div class="d-flex flex-wrap mt-1" @submit.prevent="submitQuery" id="selected-mutations">
@@ -188,11 +114,6 @@ import SARSMutationMap from "@/components/SARSMutationMap.vue";
 import {
   findPangolin
 } from "@/api/genomics.js";
-
-// --- store / Vuex ---
-import {
-  mapState
-} from "vuex";
 
 // --- font awesome --
 import {
@@ -244,24 +165,6 @@ export default Vue.extend({
     }
   },
   computed: {
-    ...mapState("genomics", ["refSeq"]),
-    selectedMax() {
-      if (this.selectedCoordinate == "aminoacid") {
-        return (this.selectedGene ? `1 - ${this.selectedGene.length}` : "select gene");
-      }
-      return (`1 - ${this.nucleotideMax}`);
-    },
-    mutationPlaceholder() {
-      return (this.selectedCoordinate == "aminoacid" ? "new AA" : "A, T, G, C")
-    },
-    selectedRef() {
-      if (this.selectedCoordinate == "aminoacid" && this.selectedGene) {
-        const selected = AA_MAP.filter(d => d.gene == this.selectedGene.name && d.codon_num == this.selectedPosition)
-        return (selected.length == 1 ? selected[0].aa : "unknown")
-      } else {
-        return (null)
-      }
-    },
     title() {
       if (this.selectedLineage) {
         return this.selectedMutations.length ? `${this.selectedLineage} lineage with ${this.selectedMutations.map(d => d.mutation).join(", ")}` : `${this.selectedLineage} lineage`;
@@ -269,26 +172,8 @@ export default Vue.extend({
         return (this.selectedMutations.length > 1 ? this.selectedMutations.map(d => d.mutation).join(", ") + " Variant" : this.selectedMutations.map(d => d.mutation).join(", ") + " Mutation")
       }
     },
-    addValid() {
-      if (this.selectedMutationType == "substitution") {
-        return (this.selectedGene && this.selectedPosition && this.selectedMutation)
-      } else if (this.selectedMutationType == "deletion") {
-        return (this.selectedGene && this.selectedPosition)
-      }
-      return (false);
-    },
     formValid() {
       return (this.selectedMutations.length > 0 || this.selectedLineage)
-    },
-    selectedLabel() {
-      if (this.addValid) {
-        if (this.selectedMutationType == "substitution") {
-          return (`${this.selectedGene.name}:${this.selectedRef}${this.selectedPosition}${this.selectedMutation}`)
-        } else if (this.selectedMutationType == "deletion") {
-          return (`${this.selectedGene.name}:DEL${this.selectedPosition}/${Number(this.selectedPosition) + Number(this.selectedDelLength) - 1}`)
-        }
-      }
-      return (null)
     }
   },
   created: function() {
@@ -309,7 +194,6 @@ export default Vue.extend({
       this.badBulkDeletion = false;
       this.badBulkGene = false;
 
-      if (this.selectedCoordinate == "aminoacid") {
         this.selectedBulkMutations = bulk.map(d => {
           const splitted = d.split(":");
           if (splitted.length == 2) {
@@ -347,40 +231,14 @@ export default Vue.extend({
           }
         })
 
-        const newMutations = this.selectedManualMutations.concat(this.selectedBulkMutations.filter(d => d));
+        const newMutations = this.selectedBulkMutations.filter(d => d);
 
         this.$emit("update:selectedMutations", newMutations);
-      }
-
-    },
-    addMutation() {
-      // Add to mutation array
-      if (this.selectedCoordinate == "aminoacid") {
-        this.selectedManualMutations.push({
-          type: this.selectedMutationType,
-          gene: this.selectedGene.name,
-          codon_num: +this.selectedPosition,
-          ref_aa: this.selectedRef,
-          alt_aa: this.selectedMutation,
-          mutation: this.selectedLabel,
-          change_length_nt: +this.selectedDelLength * 3
-        });
-      }
-      const newMutations = this.selectedManualMutations.concat(this.selectedBulkMutations)
-
-      this.$emit("update:selectedMutations", newMutations);
-      // Clear the form
-      this.selectedMutationType = null;
-      this.selectedGene = null;
-      this.selectedPosition = null;
-      this.selectedMutation = null;
     },
     deleteMutation(idx) {
       const removed = this.selectedMutations.splice(idx, 1);
       const newMutations = this.selectedMutations.filter(d => d.mutation != removed[0].mutation);
       this.$emit("update:selectedMutations", newMutations);
-      // Remove from manual mutations
-      this.selectedManualMutations = this.selectedManualMutations.filter(d => d.mutation != removed[0].mutation);
 
       // Remove from bulk mutations
       this.selectedBulkMutations = this.selectedBulkMutations.filter(d => d.mutation != removed[0].mutation);
@@ -391,13 +249,7 @@ export default Vue.extend({
       this.badBulkDeletion = false;
       this.badBulkGene = false;
       this.selectedBulkMutations = [];
-      this.selectedManualMutations = [];
       this.selectedBulkString = null;
-      this.selectedCoordinate = "aminoacid";
-      this.selectedGene = null;
-      this.selectedPosition = null;
-      this.selectedDelLength = 1;
-      this.selectedMutationType = "substitution";
       this.$emit("update:selectedLineage", null);
       this.$emit("update:selectedMutations", []);
     }
@@ -406,7 +258,6 @@ export default Vue.extend({
     return {
       queryPangolin: null,
       selectedBulkMutations: [],
-      selectedManualMutations: [],
       selectedBulkString: null,
       badBulkGene: false,
       badBulkSubstitution: false,
@@ -421,66 +272,6 @@ export default Vue.extend({
       }, {
         id: "mut",
         label: "Mutation(s)"
-      }],
-      selectedCoordinate: "aminoacid",
-      selectedGene: null,
-      selectedPosition: null,
-      selectedDelLength: 1,
-      selectedMutationType: "substitution",
-      selectedMutation: null,
-      nucleotideMax: 30000,
-      genes: [{
-        "order": 1,
-        "name": "ORF1a",
-        "length": 11501
-      }, {
-        "order": 1.5,
-        "name": "ORF1ab",
-        "length": 7096
-      }, {
-        "order": 2,
-        "name": "ORF1b",
-        "length": null
-      }, {
-        "order": 3,
-        "name": "S",
-        "length": 1273
-      }, {
-        "order": 4,
-        "name": "ORF3a",
-        "length": 275
-      }, {
-        "order": 5,
-        "name": "E",
-        "length": 75
-      }, {
-        "order": 6,
-        "name": "M",
-        "length": 222
-      }, {
-        "order": 7,
-        "name": "ORF6",
-        "length": 61
-      }, {
-        "order": 8,
-        "name": "ORF7a",
-        "length": 121
-      }, {
-        "order": 9,
-        "name": "ORF7b",
-        "length": 43
-      }, {
-        "order": 10,
-        "name": "ORF8",
-        "length": 121
-      }, {
-        "order": 11,
-        "name": "N",
-        "length": 419
-      }, {
-        "order": 12,
-        "name": "ORF10",
-        "length": 38
       }]
     }
   }
