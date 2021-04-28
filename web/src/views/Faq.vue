eeb8cf302efc2504a5175fad13efd09a8af3cf97
flaneuse <flaneuse@users.noreply.github.com>
2021-04-28 11:01:43 -0500

    loop over subgroups

--- web/src/views/Faq.vue
+++ web/src/views/Faq.vue
@@ -8,13 +8,30 @@
       <!-- automatically pull Q&A from /assets/faq.json -->
       <section id="faq" class="my-3">
         <div v-for="(group, gIdx) in faqGroups" :key="gIdx">
-          <h3 data-toggle="collapse" :href="'#group' + gIdx" class="pointer my-5 py-2 border-left-main">{{group}}</h3>
-          <ol :id="'group' + gIdx" class="collapse multi-collapse">
-            <li v-for="(question, qIdx) in faq[group]" :key="qIdx" class="mb-4 border-bottom pb-4 font-size-xlarge" :id="'g' + gIdx + '-q' + qIdx">
-              <b>{{question.q}}</b>
-              <div class="font-size-normal" v-html="question.a"></div>
-            </li>
-          </ol>
+          <h3 data-toggle="collapse" :href="'#group' + gIdx" class="pointer mt-5 py-2 border-left-main">{{group}}</h3>
+
+          <!-- one group -->
+          <template v-if="Array.isArray(faq[group])">
+            <ol :id="'group' + gIdx" class="collapse">
+              <li v-for="(question, qIdx) in faq[group]" :key="qIdx" class="mb-4 pb-4 font-size-xlarge" :class="{'border-bottom': qIdx < faq[group].length - 1}" :id="'g' + gIdx + '-q' + qIdx">
+                <b>{{question.q}}</b>
+                <div class="font-size-normal" v-html="question.a"></div>
+              </li>
+            </ol>
+          </template>
+
+          <!-- subgroups -->
+          <div :id="'group' + gIdx" class="collapse ml-4" v-else>
+            <div v-for="(subgroup, sIdx) in Object.keys(faq[group])" :key="sIdx" class="mb-5">
+              <h4 class="text-sec">{{subgroup}}</h4>
+              <ol :id="'group' + gIdx + '-subgroup' + sIdx">
+                <li v-for="(question, qIdx) in faq[group][subgroup]" :key="qIdx" class="mb-4 pb-4 font-size-xlarge" :class="{'border-bottom': qIdx < faq[group][subgroup].length - 1}" :id="'g' + gIdx + 's' + sIdx + '-q' + qIdx">
+                  <b>{{question.q}}</b>
+                  <div class="font-size-normal" v-html="question.a"></div>
+                </li>
+              </ol>
+            </div>
+          </div>
         </div>
 
       </section>
@@ -45,15 +62,15 @@ export default Vue.extend({
 
 <style lang="scss">
 .border-left-main {
-    border-left: 8px solid $secondary-color;
+    border-left: 10px solid #bfd6ef;
     padding-left: 0.75rem;
 }
 
 .font-size-xlarge {
-  font-size: larger;
+    font-size: larger;
 }
 
 .font-size-normal {
-  font-size: initial !important;
+    font-size: initial !important;
 }
 </style>
