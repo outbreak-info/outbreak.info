d4dafdf8b492a507d047a3f40c518f391b4b6271
flaneuse <flaneuse@users.noreply.github.com>
2021-07-14 13:48:06 -0700

    add checkbooks

--- web/src/views/Press.vue
+++ web/src/views/Press.vue
@@ -2,7 +2,7 @@
 <div class="bg-light d-flex flex-column justify-content-center align-items-center my-5">
   <h1 class="d-block">outbreak.info in the media</h1>
   <div id="press" class="text-left w-75">
-    <div v-for="(article, aIdx) in press" :key="aIdx" class="my-4">
+    <div v-for="(article, aIdx) in press" :key="aIdx" class="my-3 checkbook p-2">
       <a :href="article.url" target="_blank" class="font-size-2 line-height-1 no-underline">
         <div class="d-flex align-items-center">
           <img :src="require(`@/assets/press/${article.img}`)" class="article-logo mr-3" />
@@ -54,4 +54,7 @@ export default Vue.extend({
   text-decoration: underline !important;
 }
 
+.checkbook:nth-child(2n+1) {
+    background: lighten($base-grey,70%);
+}
 </style>
