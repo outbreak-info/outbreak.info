94cbcd7e5baf2d90f81e3425a700e4a9ea75c340
flaneuse <flaneuse@users.noreply.github.com>
2021-07-29 14:21:30 -0700

    setting up template for @marcodarko

---
+++ web/src/views/PageNotFound.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="half-page my-5">
+  <h1>Oops! Page not found
+  </h1>
+  <div>
+    Return to:
+    <ul class="list-unstyled">
+      <li>
+        <router-link :to="{name: 'Home'}">Home</router-link>
+      </li>
+      <li>
+        <router-link :to="{name: 'Epidemiology'}">Cases &amp; Deaths</router-link>
+      </li>
+      <li>
+        <router-link :to="{name: 'SituationReports'}">Variant Reports</router-link>
+      </li>
+      <li>
+        <router-link :to="{name: 'SituationReports'}">Research Library</router-link>
+      </li>
+    </ul>
+
+  </div>
+
+
+</div>
+</template>
