7318a9d376c6e0d11d1883a7e73240783e6676dd
flaneuse <flaneuse@users.noreply.github.com>
2022-02-11 14:47:55 -0800

    fix header/footer for iframe embeds

--- web/src/App.vue
+++ web/src/App.vue
@@ -4,103 +4,111 @@
   <!-- IMPORTANT: when editing the header, make sure you edit the footer site map below too -->
   <header id="outbreak-header">
     <nav class="navbar navbar-expand-lg navbar-dark w-100 bg-grey__lighter nav-hero">
-      <router-link to="/" class="navbar-brand no-underline">
+
+      <router-link to="/" class="navbar-brand no-underline" v-if="!$route.meta.hideNavigation">
         <img src="@/assets/icon-01.svg" width="30" height="30" class="d-inline-block align-top" alt="Outbreak.info" />
         outbreak.info
       </router-link>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav" v-if="!$route.meta.hideNavigation">
-        <ul class="navbar-nav nav-link">
-          <!-- EPIDEMIOLOGY -->
-          <li class="dropdown px-3 nav-link">
-            <div class="dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Cases &amp; Deaths
-            </div>
-            <div class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton">
-              <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" :class="{ active: $route.name == 'Epidemiology' }" :to="{
+
+      <!-- Add hard coded link to outbreak.info for the minimalistic nav bar for embeds -->
+      <a href="https://outbreak.info" class="navbar-brand no-underline" v-else>
+        <img src="@/assets/icon-01.svg" width="30" height="30" class="d-inline-block align-top" alt="Outbreak.info" />
+        outbreak.info
+      </a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav" v-if="!$route.meta.hideNavigation">
+          <ul class="navbar-nav nav-link">
+            <!-- EPIDEMIOLOGY -->
+            <li class="dropdown px-3 nav-link">
+              <div class="dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Cases &amp; Deaths
+              </div>
+              <div class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton">
+                <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" :class="{ active: $route.name == 'Epidemiology' }" :to="{
                     name: 'Epidemiology'}">
-                Compare locations over time
-              </router-link>
-              <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" :class="{ active: $route.name == 'Maps' }" :to="{
+                  Compare locations over time
+                </router-link>
+                <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" :class="{ active: $route.name == 'Maps' }" :to="{
                     name: 'Maps'
                   }">Explore interactive maps</router-link>
 
-              <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" :class="{ active: $route.name == 'Compare' }" :to="{
+                <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" :class="{ active: $route.name == 'Compare' }" :to="{
                     name: 'Compare'
                   }">Find similar locations</router-link>
 
-              <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" :class="{ active: $route.name == 'Regions' }" :to="{
+                <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" :class="{ active: $route.name == 'Regions' }" :to="{
                     name: 'Regions'
                   }">Explore regions</router-link>
 
-              <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" :class="{ active: $route.name == 'Doubling Rates' }" :to="{
+                <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" :class="{ active: $route.name == 'Doubling Rates' }" :to="{
                     name: 'Doubling Rates',
                     query: { location: 'USA', variable: 'confirmed' }
                   }">View doubling rates</router-link>
 
-              <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" to="/data" :class="{ active: $route.name == 'Data' }">Access data tables
-              </router-link>
-            </div>
-          </li>
+                <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" to="/data" :class="{ active: $route.name == 'Data' }">Access data tables
+                </router-link>
+              </div>
+            </li>
 
-          <!-- Genomics -->
-          <li class="dropdown px-3 nav-link">
-            <div class="dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Variants
-            </div>
-            <div class="dropdown-menu dropdown-menu-dark p-1" aria-labelledby="dropdownMenuButton">
-              <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" :to="{name: 'SituationReports'}" :class="{ active: $route.name == 'SituationReports' }">Lineage | Mutation Tracker
-              </router-link>
-              <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" :to="{name: 'LocationReports'}" :class="{ active: $route.name == 'LocationReports' }">Location Tracker
-              </router-link>
-              <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" :to="{name: 'SituationReportComparison'}" :class="{ active: $route.name == 'SituationReportComparison' }">Lineage Comparison
-              </router-link>
-              <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" :to="{name: 'SituationReportCaveats'}" :class="{ active: $route.name == 'SituationReportCaveats' }">Intrepreting Reports
-              </router-link>
-            </div>
-          </li>
+            <!-- Genomics -->
+            <li class="dropdown px-3 nav-link">
+              <div class="dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Variants
+              </div>
+              <div class="dropdown-menu dropdown-menu-dark p-1" aria-labelledby="dropdownMenuButton">
+                <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" :to="{name: 'SituationReports'}" :class="{ active: $route.name == 'SituationReports' }">Lineage | Mutation Tracker
+                </router-link>
+                <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" :to="{name: 'LocationReports'}" :class="{ active: $route.name == 'LocationReports' }">Location Tracker
+                </router-link>
+                <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" :to="{name: 'SituationReportComparison'}" :class="{ active: $route.name == 'SituationReportComparison' }">Lineage Comparison
+                </router-link>
+                <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" :to="{name: 'SituationReportCaveats'}" :class="{ active: $route.name == 'SituationReportCaveats' }">Intrepreting Reports
+                </router-link>
+              </div>
+            </li>
 
-          <!-- RESOURCES -->
-          <li class="dropdown px-3 nav-link">
-            <div class="dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Research Library
-            </div>
-            <div class="dropdown-menu dropdown-menu-dark p-1" aria-labelledby="dropdownMenuButton">
-              <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" to="/resources" :class="{ active: $route.name == 'Resources' }">Find research
-              </router-link>
-              <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" :to="{name: 'Sources', hash: '#resources'}" :class="{ active: $route.name == 'Sources' }">Download metadata
-              </router-link>
-              <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" to="/schema" :class="{ active: $route.name == 'Schema' }">View &amp; adapt schema
-              </router-link>
-            </div>
-          </li>
+            <!-- RESOURCES -->
+            <li class="dropdown px-3 nav-link">
+              <div class="dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Research Library
+              </div>
+              <div class="dropdown-menu dropdown-menu-dark p-1" aria-labelledby="dropdownMenuButton">
+                <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" to="/resources" :class="{ active: $route.name == 'Resources' }">Find research
+                </router-link>
+                <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" :to="{name: 'Sources', hash: '#resources'}" :class="{ active: $route.name == 'Sources' }">Download metadata
+                </router-link>
+                <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" to="/schema" :class="{ active: $route.name == 'Schema' }">View &amp; adapt schema
+                </router-link>
+              </div>
+            </li>
 
-          <!-- API -->
-          <li class="nav-item">
-            <a class="nav-link" href="https://api.outbreak.info/">API</a>
-          </li>
+            <!-- API -->
+            <li class="nav-item">
+              <a class="nav-link" href="https://api.outbreak.info/">API</a>
+            </li>
 
-          <!-- ABOUT -->
-          <li class="dropdown px-3 nav-link">
-            <div class="dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              About
-            </div>
-            <div class="dropdown-menu dropdown-menu-dark p-1" aria-labelledby="dropdownMenuButton">
-              <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" to="/about" :class="{ active: $route.name == 'About' }">About</router-link>
-              <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" to="/sources" :class="{ active: $route.name == 'Sources' }">Data sources</router-link>
-              <a class="nav-link" href="https://blog.outbreak.info">Blog</a>
-              <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" to="/faq" :class="{ active: $route.name == 'Faq' }">FAQ</router-link>
-              <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" to="/latest" :class="{ active: $route.name == 'Latest' }">Latest changes</router-link>
-              <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" :class="{ active: $route.name == 'Citation' }" :to="{name: 'Citation'}">How to cite</router-link>
-              <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" :class="{ active: $route.name == 'Videos' }" :to="{name: 'Videos'}">Video demos</router-link>
-              <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" :class="{ active: $route.name == 'Press' }" :to="{name: 'Press'}">In the media</router-link>
-            </div>
-          </li>
+            <!-- ABOUT -->
+            <li class="dropdown px-3 nav-link">
+              <div class="dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                About
+              </div>
+              <div class="dropdown-menu dropdown-menu-dark p-1" aria-labelledby="dropdownMenuButton">
+                <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" to="/about" :class="{ active: $route.name == 'About' }">About</router-link>
+                <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" to="/sources" :class="{ active: $route.name == 'Sources' }">Data sources</router-link>
+                <a class="nav-link" href="https://blog.outbreak.info">Blog</a>
+                <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" to="/faq" :class="{ active: $route.name == 'Faq' }">FAQ</router-link>
+                <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" to="/latest" :class="{ active: $route.name == 'Latest' }">Latest changes</router-link>
+                <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" :class="{ active: $route.name == 'Citation' }" :to="{name: 'Citation'}">How to cite</router-link>
+                <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" :class="{ active: $route.name == 'Videos' }" :to="{name: 'Videos'}">Video demos</router-link>
+                <router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" :class="{ active: $route.name == 'Press' }" :to="{name: 'Press'}">In the media</router-link>
+              </div>
+            </li>
 
-        </ul>
-      </div>
+          </ul>
+        </div>
     </nav>
   </header>
   <transition name="fade">
@@ -109,7 +117,7 @@
 
 
   <!-- FOOTER -->
-  <footer id="outbreak-footer" class="bg-main__darker pt-4">
+  <footer id="outbreak-footer" class="bg-main__darker pt-4" v-if="!$route.meta.hideNavigation">
     <!-- sitemap -->
     <div class="w-100 d-flex justify-content-between text-left text-muted row border-bottom navbar-dark m-0 px-3 pb-3" id="footer-sitemap">
       <!-- epi group -->
