be4346028c8085725295376b663567fec9cb16b9
flaneuse <flaneuse@users.noreply.github.com>
2022-02-11 15:41:31 -0800

    WIP: move LocationReport to a separate component

--- web/src/views/GenomicsEmbed.vue
+++ web/src/views/GenomicsEmbed.vue
@@ -1,54 +1,33 @@
 <template>
-  <div class="home flex-column align-left">
-    <div v-if="loading" class="loader">
-      <font-awesome-icon class="fa-pulse fa-4x text-highlight" :icon="['fas', 'spinner']"/>
-    </div>
+<div class="home flex-column align-left">
+  <LocationReportComponent />
 
 
-    <Logos />
-  </div>
+  <Logos />
+</div>
 </template>
+
 <script>
 // @ is an alias to /src
 import Logos from "@/components/Logos.vue";
 
-// --- font awesome --
-import {
-  FontAwesomeIcon
-} from "@fortawesome/vue-fontawesome";
-import {
-  library
-} from "@fortawesome/fontawesome-svg-core";
-import {
-  faSpinner
-} from "@fortawesome/free-solid-svg-icons";
-
-library.add(faSpinner);
-
-import { mapState } from "vuex";
-
 export default {
   name: "GenomicsEmbed",
   components: {
-    Logos,
-    FontAwesomeIcon
+    LocationReportComponent: () => import( /* webpackPrefetch: true */ "@/components/LocationReportComponent.vue"),
+    Logos
   },
   props: {
-    location: String
+    loc: String
   },
   data() {
-    return {
-    };
+    return {};
   },
-  watch: {
-  },
+  watch: {},
   computed: {
-    ...mapState("admin", ["loading"])
   },
-  methods: {
-  },
-  mounted() {
-  }
+  methods: {},
+  mounted() {}
 };
 </script>
 
