2154875393006a63c3200c18fd9a1ca469de3b14
olekkorob <korobchenko.olaksandr@gmail.com>
2022-12-27 02:59:17 +0200

    chore: setup e2e

--- web/vue.config.js
+++ web/vue.config.js
@@ -9,54 +9,54 @@
 process.env.VUE_APP_VERSION = require('./package.json').version;
 
 APP_DESCRIPTION =
-    'Outbreak.info explores COVID-19 and SARS-CoV-2 data with variant surveillance reports, data on cases and deaths, and a searchable research library.';
+  'Outbreak.info explores COVID-19 and SARS-CoV-2 data with variant surveillance reports, data on cases and deaths, and a searchable research library.';
 APP_TITLE = 'outbreak.info SARS-CoV-2 data explorer';
 
 module.exports = {
-    css: {
-        loaderOptions: {
-            sass: {
-                prependData: `@import "@/styles/_global.scss";`,
-            },
-        },
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: `@import "@/styles/_global.scss";`,
+      },
     },
-    chainWebpack: (config) => {
-        const version = process.env.VUE_APP_VERSION;
-        config.output
-            .filename(`js/[name].[hash:8].${version}.js`)
-            .chunkFilename(`js/[name].[chunkhash:8].${version}.js`);
+  },
+  chainWebpack: (config) => {
+    const version = process.env.VUE_APP_VERSION;
+    config.output
+      .filename(`js/[name].[hash:8].${version}.js`)
+      .chunkFilename(`js/[name].[chunkhash:8].${version}.js`);
+  },
+  configureWebpack: {
+    // plugins,
+    optimization: {
+      splitChunks: {
+        chunks: 'async',
+        minSize: 10000,
+        maxSize: 250000,
+      },
     },
-    configureWebpack: {
-        // plugins,
-        optimization: {
-            splitChunks: {
-                chunks: 'async',
-                minSize: 10000,
-                maxSize: 250000
-            }
+    module: {
+      rules: [
+        {
+          test: /\.mjs$/,
+          include: /node_modules/,
+          type: 'javascript/auto',
         },
-        module: {
-            rules: [
-                {
-                    test: /\.mjs$/,
-                    include: /node_modules/,
-                    type: "javascript/auto"
-                },
-                {
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: "babel-loader",
-                    },
-                },
-                {
-                    test: /\.xml$/i,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: "raw-loader"
-                    }
-                },
-            ]
-        }
-    }
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+        {
+          test: /\.xml$/i,
+          exclude: /node_modules/,
+          use: {
+            loader: 'raw-loader',
+          },
+        },
+      ],
+    },
+  },
 };
