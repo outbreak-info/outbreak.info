79004a296f33d030f3b2499ab3557d8a3b38a83a
olekkorob <korobchenko.olaksandr@gmail.com>
2022-12-29 12:34:53 +0200

    chore: remove test attribute

--- web/src/components/Warning.vue
+++ web/src/components/Warning.vue
@@ -2,11 +2,9 @@
   <div
     class="alert bg-highlight text-light px-2 py-1 m-0 rounded-0 d-flex align-items-center"
     :class="{ 'scale-in-center': animate }"
-    data-cy="wrapper"
   >
     <div
       class="d-flex justify-content-center align-items-center text-center ml-1"
-      data-cy="icon-wrapper"
     >
       <font-awesome-icon
         class="pr-2 fa-2x"
@@ -16,7 +14,6 @@
     <div
       class="d-flex justify-content-center align-items-center"
       :class="[align_left ? 'text-left' : 'text-center']"
-      data-cy="text-wrapper"
     >
       <p class="m-0" v-html="text"></p>
     </div>
