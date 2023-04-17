72c74ec15fea1f5eb344569cd72748a4b4b52a7d
olekkorob <korobchenko.olaksandr@gmail.com>
2023-04-17 23:02:22 +0300

    fix: build issue

--- web/vite.config.js
+++ web/vite.config.js
@@ -6,9 +6,6 @@ const path = require('path');
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), pluginRewriteAll()],
-  define: {
-    global: 'window',
-  },
   resolve: {
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
     alias: {
@@ -30,4 +27,10 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    rollupOptions: {
+      external: ['pixi.js'],
+    },
+  },
+  assetsInclude: ['**/*.svg', '**/*.png', '**/*.jpg'],
 });
