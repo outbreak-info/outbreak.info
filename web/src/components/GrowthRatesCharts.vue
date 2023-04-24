611aee872a5582756fa46c7f472b8f49f50a0348
Leandro Collares <leco@uvic.ca>
2023-04-24 09:11:46 -0300

    feat: create child component to host charts

---
+++ web/src/components/GrowthRatesCharts.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>{{ title }}</h2>
+  </div>
+</template>
+
+<script setup>
+  const props = defineProps({
+    data: Array,
+  });
+
+  const title = `${props.data[0].lineage} grow rates in the ${props.data[0].location}`;
+
+  console.log("data received by child component", props.data);
+</script>
--- web/src/views/GrowthRatesPage.vue
+++ web/src/views/GrowthRatesPage.vue
@@ -1,42 +1,47 @@
 <template>
-  <h1>Hello there!</h1>
+  <div>
+    <h1>Growth rates</h1>
+      <GrowthRatesCharts v-if="growthData.length > 0" :data="growthData.filter((d => d.location == selectedLocation) && (d => d.lineage == selectedLineage))"/>
+  </div>
 </template>
-
+  
 <script setup>
-import { csv } from 'd3-fetch';
-
-let growthData;
-
-let country = 'United States';
-let lineage = 'BA.5.1.18';
-
-csv('../../data/mockData.csv').then(data => {
-    data.forEach(d => {
-      d.growth_rate = d.growth_rate * 100;
-      d.uncertainty = d.uncertainty * 100;
-      d.invUncertainty = +d.invUncertainty;
-      d.uncertainty95 = d.uncertainty95 * 100;
-      d.uncertainty80 = d.uncertainty80 * 100;
-      d.uncertainty65 = d.uncertainty65 * 100;
-      d.uncertainty50 = d.uncertainty50 * 100;
-      d.uncertainty35 = d.uncertainty35 * 100;
-      d.uncertainty20 = d.uncertainty20 * 100;
-      d.uncertainty5 = d.uncertainty5 * 100;
-      d.intervals = [
-        d.uncertainty5,
-        d.uncertainty20,
-        d.uncertainty35,
-        d.uncertainty50,
-        d.uncertainty65,
-        d.uncertainty80,
-        d.uncertainty95,
-      ];
-      d.prevalence = d.Prevalence_7 * 100;
-      d.sequences = d.N_7;
-      d.totalSequences = d.deltaN_7;
-      growthData = data.filter((d => d.location == country) && (d => d.lineage == lineage))
-    });
-    console.log(growthData);
-});
+  import { csv } from 'd3-fetch';
+  import { ref } from 'vue'
+  import GrowthRatesCharts from '@/components/GrowthRatesCharts.vue';
+  
+  let growthData = ref([]);
+  
+  let selectedLocation = 'United States';
+  let selectedLineage = 'BA.5.1.18';
+  
+  csv('../../data/mockData.csv').then(data => {
+      data.forEach(d => {
+        d.growth_rate = d.growth_rate * 100;
+        d.uncertainty = d.uncertainty * 100;
+        d.invUncertainty = +d.invUncertainty;
+        d.uncertainty95 = d.uncertainty95 * 100;
+        d.uncertainty80 = d.uncertainty80 * 100;
+        d.uncertainty65 = d.uncertainty65 * 100;
+        d.uncertainty50 = d.uncertainty50 * 100;
+        d.uncertainty35 = d.uncertainty35 * 100;
+        d.uncertainty20 = d.uncertainty20 * 100;
+        d.uncertainty5 = d.uncertainty5 * 100;
+        d.intervals = [
+          d.uncertainty5,
+          d.uncertainty20,
+          d.uncertainty35,
+          d.uncertainty50,
+          d.uncertainty65,
+          d.uncertainty80,
+          d.uncertainty95,
+        ];
+        d.prevalence = d.Prevalence_7 * 100;
+        d.sequences = d.N_7;
+        d.totalSequences = d.deltaN_7;
+      });
+      growthData.value = data;
+      console.log("afterAssignment", growthData.value);
+  });
 </script>
-
+  
