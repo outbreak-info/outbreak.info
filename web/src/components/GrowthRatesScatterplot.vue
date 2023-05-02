b4d9b37ebf257c431fbe1a5df053c40c8ac8826b
Leandro Collares <leco@uvic.ca>
2023-05-02 18:15:01 -0300

    feat: add event listeners to scatterplot's points

--- web/src/components/GrowthRatesScatterplot.vue
+++ web/src/components/GrowthRatesScatterplot.vue
@@ -15,6 +15,16 @@
           :yScale="yScale"
           :innerWidth="innerWidth"
         />
+        <rect
+          :width="innerWidth"
+          :height="innerHeight"
+          :x="0"
+          :y="0"
+          fill="#ffffff"
+          fill-opacity="0"
+          @mousemove="handleMouseMove"
+          @mouseleave="handleMouseLeave"
+        />
         <circle 
           v-for="(dataPoint, index) in data" :key="'point-' + index"
           :r="xScale.bandwidth() / 2"
@@ -24,14 +34,24 @@
           stroke="#2c3e50"
           stroke-width="1px"
         />
+        <circle
+          v-if="hoveredPoint"
+          :r="(xScale.bandwidth() / 2) + 3"
+          :cx="xAccessorScaled(hoveredPoint)"
+          :cy="yAccessorScaled(hoveredPoint)"
+          fill="none" 
+          stroke="#2c3e50"
+          stroke-width="2px"
+        />
       </g>
     </svg>
   </div>
 </template>
 
 <script setup>
-  import { computed } from 'vue'
+  import { computed, ref } from 'vue'
   import GrowthRatesYAxis from '@/components/GrowthRatesYAxis.vue';
+  import { quadtree } from "https://cdn.skypack.dev/d3-quadtree@3";
 
   const props = defineProps({
     loc: String,
@@ -50,7 +70,26 @@
 
   console.log("scatter loc", props.loc);
   console.log("scatter data", props.data);
+
+  const hoveredPoint = ref(null);
  
   const xAccessorScaled = computed(() => d => props.xScale(props.xAccessor(d)));
   const yAccessorScaled = computed(() => d => props.yScale(props.yAccessor(d)));
+
+  const quadtreeInstance = computed (() => quadtree()
+    .x(d => props.xScale(props.xAccessor(d)))
+    .y(d => props.yScale(props.yAccessor(d)))
+    .addAll(props.data)
+  );
+
+  const handleMouseMove = (e) => {
+    const xPosition = e.offsetX - props.margin.left;
+    const yPosition = e.offsetY - props.margin.top;
+    hoveredPoint.value = quadtreeInstance.value.find(xPosition, yPosition);
+    console.log("hoveredPoint", hoveredPoint.value);
+  }
+
+  const handleMouseLeave = () => {
+    hoveredPoint.value = null;
+  }
 </script>
