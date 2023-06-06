3fd20bab8e865e723e8b05095faab84b509ddfb5
Leandro Collares <leco@uvic.ca>
2023-06-06 11:54:14 -0300

    feat: improve missing data messages

--- web/src/components/GrowthRatesWarning.vue
+++ web/src/components/GrowthRatesWarning.vue
@@ -15,7 +15,7 @@
 
   const locationCount = ref(props.data.length);
 
-  const titleString = `No API data for ${locationCount.value} ${locationCount.value > 1 ? "locations" : "location"}`;
+  const titleString = `No data for ${locationCount.value} ${locationCount.value > 1 ? "locations" : "location"}`;
 
   const locationNamesArray = ref((props.data.map(obj => obj.label)).sort());
 
@@ -23,8 +23,8 @@
     `${locationNamesArray.value.slice(0,-1).join(', ')} and ${locationNamesArray.value.slice(-1)}`
     : locationNamesArray.value.toString();
     
-  const contentString = `${props.lineage} growth rates not available for ${locationNamesString}`;
-
+  const contentString = `${props.lineage} growth rates not available for ${locationNamesString}. Sequence counts by variant, case and death counts and population are used to compute growth rates. These data sources are not always available.`;
+ 
   onMounted(() => {
     showMessage();
   });
