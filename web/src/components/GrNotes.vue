99134120e1a652fb02dffd011677f0b2773409f1
Leandro Collares <leco@uvic.ca>
2023-06-27 09:13:29 -0300

    feat: add detailed info on growth rate calculations

--- web/src/components/GrNotes.vue
+++ web/src/components/GrNotes.vue
@@ -9,6 +9,14 @@
       <p>
         Growth rates are calculated by comparing the number of sequences lineage detected in a location to the number of sequences of other lineages, and to the same quantities from the previous week. Simple models are used to correct for local variation in reporting conditions based on population and reported case and death counts, and to estimate the uncertainty in our growth-rates measurements.
       </p>
+      <p>
+        <a 
+          href="/src/assets/growth-rates/growth_rates.pdf"
+          target="_blank"
+        >
+          Detailed information on growth rate calculations
+        </a> 
+      </p>
       <h3>Growth rate visualizations</h3>
       <p>
         For each lineage-location pair, we employ a visualization comprised of a <span class="bold">scatterplot</span> and a <span class="bold">line-stripe chart</span> to display growth rate data. Scatterplots help visitors compare growth rates visually, whereas stripes illuminate data trends. 
@@ -40,7 +48,7 @@
       />
       <h3>Data gaps</h3>
       <p>
-        Growth rates are only calculated when a lineage is detected in sequence data from a location consistently over a one-week interval; as a result of limitations in sequencing coverage, gaps may exist in our growth-rates data estimates, especially for low-prevalence lineages. It is appropriate to assume that lineage growth rates vary continuously over time, and tend towards zero during sequencing gaps. Please refer to this document for more details.
+        Growth rates are only calculated when a lineage is detected in sequence data from a location consistently over a one-week interval; as a result of limitations in sequencing coverage, gaps may exist in our growth-rates data estimates, especially for low-prevalence lineages. It is appropriate to assume that lineage growth rates vary continuously over time, and tend towards zero during sequencing gaps. 
       </p>
       <p>
         Gaps may also result from design choices as we remove outliers to keep the y-scale of the scatterplots constant and make growth rate variations over time more noticeable.
